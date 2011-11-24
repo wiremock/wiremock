8c0a219e23bc6cb5c781eca0f3059009fc9d94da
Tom Akehurst <t.m.akehurst@googlemail.com>
2011-11-24 21:08:10 +0000

    Revert "Switched mapping predicate to use named inner class rather than anon to avoid risk of permgen depletion"
    This reverts commit bf976a7b6de6abe8cc35130029f270c6252ce14b.

--- src/main/java/com/tomakehurst/wiremock/mapping/InMemoryMappings.java
+++ src/main/java/com/tomakehurst/wiremock/mapping/InMemoryMappings.java
@@ -4,6 +4,8 @@ import static com.google.common.collect.Iterables.find;
 
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import com.google.common.base.Predicate;
+
 
 public class InMemoryMappings implements Mappings {
 	
@@ -11,12 +13,18 @@ public class InMemoryMappings implements Mappings {
 	
 	@Override
 	public Response getFor(Request request) {
-		RequestResponseMapping matchingMapping = find(requestResponseMappings,
-				RequestResponseMapping.Matcher.forRequest(request),
-				RequestResponseMapping.notConfigured());
+		RequestResponseMapping matchingMapping = find(requestResponseMappings, mappingMatching(request), RequestResponseMapping.notConfigured());
 		return matchingMapping.getResponse();
 	}
 	
+	private Predicate<RequestResponseMapping> mappingMatching(final Request request) {
+		return new Predicate<RequestResponseMapping>() {
+			public boolean apply(RequestResponseMapping input) {
+				return input.getRequest().isMatchedBy(request);
+			}
+		};
+	}
+
 	@Override
 	public void addMapping(RequestResponseMapping mapping) {
 		requestResponseMappings.add(0, mapping);
