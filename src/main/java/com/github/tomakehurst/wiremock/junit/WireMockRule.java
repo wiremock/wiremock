ac1a3ee983a2bacbe9ae66706399656b10d6a227
pimterry <pimterry@gmail.com>
2012-12-29 00:35:42 +0000

    Tweaked WireMockRule so it can now also be run as a @ClassRule in JUnit >= 4.9, keeping the server alive between individual tests.

--- src/main/java/com/github/tomakehurst/wiremock/junit/WireMockRule.java
+++ src/main/java/com/github/tomakehurst/wiremock/junit/WireMockRule.java
@@ -16,41 +16,57 @@
 package com.github.tomakehurst.wiremock.junit;
 
 import org.junit.rules.MethodRule;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.Statement;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 
-public class WireMockRule implements MethodRule {
-	
-	private int port;
-	
-	public WireMockRule(int port) {
-		this.port = port;
-	}
-	
-	public WireMockRule() {
-		this(WireMockServer.DEFAULT_PORT);
-	}
+public class WireMockRule implements MethodRule, TestRule {
 
-	@Override
-	public Statement apply(final Statement base, FrameworkMethod method, Object target) {
-		return new Statement() {
+    private int port;
+    private final WireMockServer wireMockServer;
 
-			@Override
-			public void evaluate() throws Throwable {
-				WireMockServer wireMockServer = new WireMockServer(port);
-				wireMockServer.start();
-				WireMock.configureFor("localhost", port);
-				try {
-                    base.evaluate();
-                } finally {
-                    wireMockServer.stop();
+    public WireMockRule(int port) {
+        this.port = port;
+        this.wireMockServer = new WireMockServer(port);
+    }
+
+    public WireMockRule() {
+        this(WireMockServer.DEFAULT_PORT);
+    }
+
+    @Override
+    public Statement apply(final Statement base, FrameworkMethod method, Object target) {
+        return apply(base, null);
+    }
+
+    @Override
+    public Statement apply(final Statement base, Description description) {
+        return new Statement() {
+
+            @Override
+            public void evaluate() throws Throwable {
+                if (wireMockServer.isRunning()) {
+                    try {
+                        base.evaluate();
+                    } finally {
+                        WireMock.reset();
+                    }
+                } else {
+                    wireMockServer.start();
+                    WireMock.configureFor("localhost", port);
+                    try {
+                        base.evaluate();
+                    } finally {
+                        wireMockServer.stop();
+                    }
                 }
-			}
-			
-		};
-	}
+            }
+
+        };
+    }
 
 }
