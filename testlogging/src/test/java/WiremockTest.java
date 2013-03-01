7718bc5c4075b3daec7fda518e04aab9e37fe06d
Tom Akehurst <t.m.akehurst@googlemail.com>
2013-03-01 20:53:59 +0000

    Now asserts that logback messages end up in stdout

--- testlogging/src/test/java/WiremockTest.java
+++ testlogging/src/test/java/WiremockTest.java
@@ -1,8 +1,12 @@
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
 import java.net.URL;
 
 import org.apache.commons.io.IOUtils;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
@@ -11,19 +15,50 @@ import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class WiremockTest {
 
     @Rule
     public WireMockRule wiremock = new WireMockRule();
 
+    StringPrintStream stdOutCapture;
+    PrintStream stdOut;
+
+    @Before
+    public void captureStdOut() {
+        stdOut = System.out;
+        stdOutCapture = new StringPrintStream();
+        System.setOut(stdOutCapture);
+    }
+
+    @After
+    public void revertStdOut() {
+        System.setOut(stdOut);
+    }
+
+
     @Test
     public void useWireMock() throws IOException {
         stubFor(get(urlMatching("/blah")).willReturn(aResponse().withStatus(200).withBody("body")));
         URL uri = new URL("http://localhost:8080/blah");
         InputStream content = uri.openConnection().getInputStream();
+
         final String retrievedBody = IOUtils.toString(content);
         assertEquals("body", retrievedBody);
+        assertThat(stdOutCapture.toString(), containsString("c.g.t.wiremock.common.Log4jNotifier - Received request to /mappings/new"));
+    }
+
+    private static class StringPrintStream extends PrintStream {
+        public StringPrintStream() {
+            super(new ByteArrayOutputStream());
+        }
+
+        @Override
+        public String toString() {
+            return out.toString();
+        }
     }
 }
