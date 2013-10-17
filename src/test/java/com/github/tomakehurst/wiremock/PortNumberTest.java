5c333950112adb4bce692e773bf0eec1556612fb
Neil Green <neil@melessoftware.com>
2013-10-17 17:26:34 +0100

    expose listening ports as properties

--- src/test/java/com/github/tomakehurst/wiremock/PortNumberTest.java
+++ src/test/java/com/github/tomakehurst/wiremock/PortNumberTest.java
@@ -15,25 +15,80 @@
  */
 package com.github.tomakehurst.wiremock;
 
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.testsupport.MappingJsonSamples;
 import com.github.tomakehurst.wiremock.testsupport.WireMockResponse;
 import com.github.tomakehurst.wiremock.testsupport.WireMockTestClient;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class PortNumberTest {
 
-	@Test
-	public void canRunOnAnotherPortThan8080() {
-		WireMockServer wireMockServer = new WireMockServer(8090);
-		wireMockServer.start();
-		WireMockTestClient wireMockClient = new WireMockTestClient(8090);
-		
-		wireMockClient.addResponse(MappingJsonSamples.BASIC_MAPPING_REQUEST_WITH_RESPONSE_HEADER);
-		WireMockResponse response = wireMockClient.get("/a/registered/resource");
-		assertThat(response.statusCode(), is(401));
-		
-	}
+    private List<WireMockServer> createdServers;
+
+    @Before
+    public void setup() {
+        createdServers = new ArrayList<WireMockServer>();
+    }
+
+    @After
+    public void stopServers() {
+        for (WireMockServer wireMockServer : createdServers) {
+            if(wireMockServer.isRunning()) {
+                wireMockServer.stop();
+            }
+        }
+    }
+
+    @Test
+    public void canRunOnAnotherPortThan8080() {
+        WireMockServer wireMockServer = createServer(wireMockConfig().port(8090));
+        wireMockServer.start();
+        WireMockTestClient wireMockClient = new WireMockTestClient(8090);
+
+        wireMockClient.addResponse(MappingJsonSamples.BASIC_MAPPING_REQUEST_WITH_RESPONSE_HEADER);
+        WireMockResponse response = wireMockClient.get("/a/registered/resource");
+        assertThat(response.statusCode(), is(401));
+    }
+
+
+    @Test
+    public void configurePortIsReportedListeningPort() {
+        WireMockServer wireMockServer = createServer(wireMockConfig().port(8091).httpsPort(8092));
+        wireMockServer.start();
+        assertThat("server", wireMockServer, hasProperty("listeningHttpPort", equalTo(8091)));
+        assertThat("server", wireMockServer, hasProperty("listeningHttpsPort", equalTo(8092)));
+    }
+
+    @Test
+    public void unstartedServerReportsListeningOnNegativeOne() {
+        WireMockServer wireMockServer = createServer(wireMockConfig().port(8091).httpsPort(8092));
+        assertThat("server", wireMockServer, hasProperty("listeningHttpPort", equalTo(-1)));
+        assertThat("server", wireMockServer, hasProperty("listeningHttpsPort", equalTo(-1)));
+    }
+
+    @Test
+    public void stoppedServerReportsListeningOnNegativeOne() {
+        WireMockServer wireMockServer = createServer(wireMockConfig().port(8091).httpsPort(8092));
+        wireMockServer.start();
+        wireMockServer.stop();
+        assertThat("server", wireMockServer, hasProperty("listeningHttpPort", equalTo(-1)));
+        assertThat("server", wireMockServer, hasProperty("listeningHttpsPort", equalTo(-1)));
+    }
+
+    private WireMockServer createServer(WireMockConfiguration configuration) {
+        final WireMockServer wireMockServer = new WireMockServer(configuration);
+        createdServers.add(wireMockServer);
+        return wireMockServer;
+    }
 }
