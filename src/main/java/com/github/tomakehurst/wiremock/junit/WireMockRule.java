2d5d167004f10a9f82b2452a18fa0290000db84a
Neil Green <neil@melessoftware.com>
2013-10-19 13:19:27 +0100

    document return values for listening port methods on junit rules

--- src/main/java/com/github/tomakehurst/wiremock/junit/WireMockRule.java
+++ src/main/java/com/github/tomakehurst/wiremock/junit/WireMockRule.java
@@ -114,10 +114,16 @@ public class WireMockRule implements MethodRule, TestRule, Stubbing {
         wireMock.addDelayBeforeProcessingRequests(milliseconds);
     }
 
+    /**
+     * @return the port the http connector is listening on, or -1 if it's not listening
+     */
     public int getListeningHttpPort() {
         return wireMockServer == null ? -1 : wireMockServer.getListeningHttpPort();
     }
 
+    /**
+     * @return the port the https connector is listening on, or -1 if it's not listening
+     */
     public int getListeningHttpsPort() {
         return wireMockServer == null ? -1 : wireMockServer.getListeningHttpsPort();
     }
