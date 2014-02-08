bddd1d23a1b4790f1f0031f7a6f0b5600cb5f411
shwet Shashank <shwet.shashank@flipkart.com>
2014-02-08 18:37:16 +0530

    changed request method from PUT to DELETE for remove mapping

--- src/main/java/com/github/tomakehurst/wiremock/admin/AdminTasks.java
+++ src/main/java/com/github/tomakehurst/wiremock/admin/AdminTasks.java
@@ -21,9 +21,7 @@ import com.google.common.collect.ImmutableBiMap;
 
 import static com.github.tomakehurst.wiremock.admin.RequestSpec.requestSpec;
 import static com.github.tomakehurst.wiremock.common.Exceptions.throwUnchecked;
-import static com.github.tomakehurst.wiremock.http.RequestMethod.GET;
-import static com.github.tomakehurst.wiremock.http.RequestMethod.POST;
-import static com.github.tomakehurst.wiremock.http.RequestMethod.PUT;
+import static com.github.tomakehurst.wiremock.http.RequestMethod.*;
 
 public class AdminTasks {
 
@@ -41,7 +39,7 @@ public class AdminTasks {
                 .put(requestSpec(POST, "/socket-delay"), SocketDelayTask.class)
                 .put(requestSpec(POST, "/settings"), GlobalSettingsUpdateTask.class)
                 .put(requestSpec(POST, "/shutdown"), ShutdownServerTask.class)
-                .put(requestSpec(PUT, "/mappings/remove"), RemoveMappingTask.class)
+                .put(requestSpec(DELETE, "/mappings/remove"), RemoveMappingTask.class)
                 .build();
 
     public static AdminTask taskFor(RequestMethod method, String path) {
