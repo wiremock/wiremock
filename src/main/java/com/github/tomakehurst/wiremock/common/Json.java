65352b218d0eabf19ac7565a564e38f72c746550
shwet Shashank <shwet.shashank@flipkart.com>
2014-02-10 09:27:07 +0530

    making object mappers in Json static

--- src/main/java/com/github/tomakehurst/wiremock/common/Json.java
+++ src/main/java/com/github/tomakehurst/wiremock/common/Json.java
@@ -24,11 +24,19 @@ public final class Json {
 	
 	private Json() {}
 
+    private static ObjectMapper readMapper;
+    private static ObjectMapper writeMapper;
+
+    static {
+        readMapper = new ObjectMapper();
+        writeMapper = new ObjectMapper();
+        readMapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
+    }
+
     public static <T> T read(String json, Class<T> clazz) {
 		try {
-			ObjectMapper mapper = new ObjectMapper();
-			mapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
-			return mapper.readValue(json, clazz);
+			readMapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
+			return readMapper.readValue(json, clazz);
 		} catch (IOException ioe) {
 			throw new RuntimeException("Unable to bind JSON to object. Reason: " + ioe.getMessage() + "  JSON:" + json, ioe);
 		}
@@ -36,8 +44,7 @@ public final class Json {
 	
 	public static <T> String write(T object) {
 		try {
-			ObjectMapper mapper = new ObjectMapper();
-			return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(object);
+			return writeMapper.writerWithDefaultPrettyPrinter().writeValueAsString(object);
 		} catch (IOException ioe) {
 			throw new RuntimeException("Unable to generate JSON from object. Reason: " + ioe.getMessage(), ioe);
 		}
