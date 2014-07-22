7750df26dae03782480c791e329f0a0ba5f015c4
Christian Amann <christian.amann@qaware.de>
2014-07-22 13:19:48 +0200

    Null check if parameter is not present

--- src/main/java/com/github/tomakehurst/wiremock/servlet/HandlerDispatchingServlet.java
+++ src/main/java/com/github/tomakehurst/wiremock/servlet/HandlerDispatchingServlet.java
@@ -72,6 +72,9 @@ public class HandlerDispatchingServlet extends HttpServlet {
      */
     private String getNormalizedMappedUnder(ServletConfig config) {
         String mappedUnder = config.getInitParameter(MAPPED_UNDER_KEY);
+        if(mappedUnder == null) {
+            return null;
+        }
         if (mappedUnder.endsWith("/")) {
             mappedUnder = mappedUnder.substring(0, mappedUnder.length() - 1);
         }
