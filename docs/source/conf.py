72662c0f78002caf87dd35a7284c4bac080102f1
Tom Akehurst <t.m.akehurst@googlemail.com>
2014-08-02 12:37:31 +0100

    Bumped version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ copyright = u'2012, Tom Akehurst'
 # built documents.
 #
 # The short X.Y version.
-version = '1.46'
+version = '1.47'
 # The full version, including alpha/beta/rc tags.
 release = version
 
