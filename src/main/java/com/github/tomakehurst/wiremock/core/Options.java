b07af66804c814e804aeaee9f442721f4a6e375c
Christian Amann <christian.amann@qaware.de>
2014-12-11 13:50:04 +0100

    Add comments to Options interface

--- src/main/java/com/github/tomakehurst/wiremock/core/Options.java
+++ src/main/java/com/github/tomakehurst/wiremock/core/Options.java
@@ -23,22 +23,76 @@ import com.github.tomakehurst.wiremock.common.Notifier;
 import com.github.tomakehurst.wiremock.common.ProxySettings;
 import com.github.tomakehurst.wiremock.http.CaseInsensitiveKey;
 
+/**
+ * Options for running wiremock. More information on most options can be found in the manual (Stand-Alone operation)
+ */
 public interface Options {
 
     public static final int DEFAULT_PORT = 8080;
     public static final String DEFAULT_BIND_ADDRESS = "0.0.0.0";
 
+    /**
+     * @return Port number on which wiremock is listening
+     */
     int portNumber();
+
+    /**
+     * @return Settings for HTTPS connections to wiremock
+     */
     HttpsSettings httpsSettings();
-    boolean browserProxyingEnabled();
-    ProxySettings proxyVia();
+
+    /**
+     * @return File source that supplies initial configuration files for wiremock.
+     */
     FileSource filesRoot();
+
+    /**
+     * @return Logger for warnings and errors
+     */
     Notifier notifier();
+
+    /**
+     * @return true if request journal is disabled (see manual)
+     */
     boolean requestJournalDisabled();
+
+    /**
+     * @return Maximum size of request journal (see manual)
+     */
     Integer maxEntriesRequestJournal();
+
+    /**
+     * @return Address to bind to. Determines from which hosts wiremock accepts connection requests.
+     */
     public String bindAddress();
+
+    /**
+     * @return Enable browser proxying (see manual)
+     */
+    boolean browserProxyingEnabled();
+
+    /**
+     * @return Settings for browser proxying (see manual)
+     */
+    ProxySettings proxyVia();
+
+    /**
+     * @return Headers to capture in record mode
+     */
     List<CaseInsensitiveKey> matchingHeaders();
+
+    /**
+     * @return Proxy all requests to another base URL. Usually used in record mode.
+     */
     public String proxyUrl();
+
+    /**
+     * @return Preserve host header in record mode
+     */
     public boolean shouldPreserveHostHeader();
+
+    /**
+     * @return Host header to use when forwarding requests in record mode.
+     */
     String proxyHostHeader();
 }
