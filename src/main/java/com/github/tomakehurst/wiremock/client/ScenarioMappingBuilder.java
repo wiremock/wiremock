8798be2328fd793657b030a09ed78731b3ead31e
Rowan Hill <rowan.hill@softwire.com>
2016-01-04 20:34:38 +0000

    ScenarioMappingBuilder is no longer generic, does not override methods
    The previously overridden methods are redundant; the actual interface of
    ScenarioMappingBuilder is unchanged.

--- src/main/java/com/github/tomakehurst/wiremock/client/ScenarioMappingBuilder.java
+++ src/main/java/com/github/tomakehurst/wiremock/client/ScenarioMappingBuilder.java
@@ -15,16 +15,7 @@
  */
 package com.github.tomakehurst.wiremock.client;
 
-public interface ScenarioMappingBuilder<S extends ScenarioMappingBuilder> extends RemoteMappingBuilder<S, S> {
-
-    S atPriority(Integer priority);
-    S withHeader(String key, ValueMatchingStrategy headerMatchingStrategy);
-    S withQueryParam(String key, ValueMatchingStrategy queryParamMatchingStrategy);
-    S withRequestBody(ValueMatchingStrategy bodyMatchingStrategy);
-    S inScenario(String scenarioName);
-
-    S willReturn(ResponseDefinitionBuilder responseDefBuilder);
-
-    S whenScenarioStateIs(String stateName);
-    S willSetStateTo(String stateName);
+public interface ScenarioMappingBuilder extends RemoteMappingBuilder<ScenarioMappingBuilder, ScenarioMappingBuilder> {
+    ScenarioMappingBuilder whenScenarioStateIs(String stateName);
+    ScenarioMappingBuilder willSetStateTo(String stateName);
 }
