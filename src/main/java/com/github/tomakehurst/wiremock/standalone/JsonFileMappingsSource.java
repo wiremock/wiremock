a093d20ddc15cb6d1f9871d06d2878c7839a7058
Dipanjan Laha <dlaha@atlassian.com>
2016-01-28 09:07:21 +1100

    Save edited mapping to the file it was loaded from

---
+++ src/main/java/com/github/tomakehurst/wiremock/standalone/JsonFileMappingsSource.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2011 Thomas Akehurst
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.tomakehurst.wiremock.standalone;
+
+import com.github.tomakehurst.wiremock.common.FileSource;
+import com.github.tomakehurst.wiremock.common.TextFile;
+import com.github.tomakehurst.wiremock.common.VeryShortIdGenerator;
+import com.github.tomakehurst.wiremock.stubbing.JsonStubMappingCreator;
+import com.github.tomakehurst.wiremock.stubbing.StubMapping;
+import com.github.tomakehurst.wiremock.stubbing.StubMappings;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static com.github.tomakehurst.wiremock.common.Json.write;
+import static com.google.common.collect.Iterables.filter;
+
+public class JsonFileMappingsSource implements MappingsSource {
+
+	private final FileSource mappingsFileSource;
+	private final VeryShortIdGenerator idGenerator;
+	private final Map<UUID, String> fileNameMap;
+
+	public JsonFileMappingsSource(FileSource mappingsFileSource) {
+		this.mappingsFileSource = mappingsFileSource;
+		idGenerator = new VeryShortIdGenerator();
+		fileNameMap = new HashMap<>();
+	}
+
+	@Override
+	public void saveMappings(StubMappings stubMappings) {
+		Collection<StubMapping> transientStubs = Collections2.filter(stubMappings.getAll(), new Predicate<StubMapping>() {
+			public boolean apply(StubMapping input) {
+				return input != null && input.isTransient();
+			}
+		});
+
+		for (StubMapping mapping : transientStubs) {
+			String mappingFileName = fileNameMap.get(mapping.getUuid());
+			if (mappingFileName == null) {
+				mappingFileName = "saved-mapping-" + idGenerator.generate() + ".json";
+			}
+			mappingsFileSource.writeTextFile(mappingFileName, write(mapping));
+			mapping.setTransient(false);
+		}
+	}
+
+	@Override
+	public void loadMappingsInto(StubMappings stubMappings) {
+		if (!mappingsFileSource.exists()) {
+			return;
+		}
+		Iterable<TextFile> mappingFiles = filter(mappingsFileSource.listFilesRecursively(), byFileExtension("json"));
+		for (TextFile mappingFile: mappingFiles) {
+			StubMapping mapping = new JsonStubMappingCreator().createMappingFrom(mappingFile.readContentsAsString());
+			stubMappings.addMapping(mapping);
+			fileNameMap.put(mapping.getUuid(), getFileName(mappingFile));
+		}
+	}
+	
+	private Predicate<TextFile> byFileExtension(final String extension) {
+		return new Predicate<TextFile>() {
+			public boolean apply(TextFile input) {
+				return input.name().endsWith("." + extension);
+			}
+		};
+	}
+
+	private String getFileName(TextFile mappingFile) {
+		return StringUtils.removeStart(mappingFile.getUri().getPath(), mappingsFileSource.getUri().getPath());
+	}
+}
---
+++ src/test/java/com/github/tomakehurst/wiremock/standalone/JsonFileMappingsSourceTest.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (C) 2011 Thomas Akehurst
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.tomakehurst.wiremock.standalone;
+
+import com.github.tomakehurst.wiremock.common.FileSource;
+import com.github.tomakehurst.wiremock.common.Json;
+import com.github.tomakehurst.wiremock.common.TextFile;
+import com.github.tomakehurst.wiremock.stubbing.InMemoryStubMappings;
+import com.github.tomakehurst.wiremock.stubbing.StubMapping;
+import com.github.tomakehurst.wiremock.stubbing.StubMappings;
+import com.google.common.collect.Lists;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.net.URI;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class JsonFileMappingsSourceTest {
+
+	private Mockery context;
+
+	private FileSource mappingsFileSource;
+	private JsonFileMappingsSource jsonFileMappingsSource;
+
+	@Before
+	public void setUp() throws Exception {
+		context = new Mockery();
+		mappingsFileSource = context.mock(FileSource.class, "mappingFileSource");
+		jsonFileMappingsSource = new JsonFileMappingsSource(mappingsFileSource);
+	}
+
+	@Test
+	public void testLoadMappings() throws Exception {
+
+		final StubMappings stubMappings = new InMemoryStubMappings();
+
+		final String mappingFileName = "testmapping.json";
+		final URI fileUri = this.getClass().getResource("/mappings/" + mappingFileName).toURI();
+		final TextFile textFile = new TextFile(fileUri);
+
+		final String mappingFileSource = fileUri.getPath().replace("/" + mappingFileName, "");
+
+
+		context.checking(new Expectations() {{
+			atLeast(1).of(mappingsFileSource).exists();
+			will(returnValue(true));
+			oneOf(mappingsFileSource).listFilesRecursively();
+			will(returnValue(Lists.newArrayList(textFile)));
+			oneOf(mappingsFileSource).getUri();
+			will(returnValue(URI.create(mappingFileSource)));
+		}});
+
+		jsonFileMappingsSource.loadMappingsInto(stubMappings);
+
+		assertThat(stubMappings.getAll(), hasSize(1));
+	}
+
+	@Test
+	public void testSaveNewMappings() throws Exception {
+
+		final StubMapping stubMapping = new StubMapping();
+		stubMapping.setTransient(true);
+
+		final StubMappings stubMappings = new InMemoryStubMappings();
+		stubMappings.addMapping(stubMapping);
+
+		context.checking(new Expectations() {{
+			one(mappingsFileSource).writeTextFile(
+					with(allOf(startsWith("saved-mapping-"), endsWith(".json"))),
+					with(Json.write(stubMapping))
+			);
+		}});
+
+		jsonFileMappingsSource.saveMappings(stubMappings);
+		assertThat(stubMapping.isTransient(), is(false));
+	}
+
+	@Test
+	public void testSaveEditedMappings() throws Exception {
+
+		final StubMappings stubMappings = new InMemoryStubMappings();
+
+		final String mappingFileName = "/testmapping.json";
+		final URI fileUri = this.getClass().getResource("/mappings" + mappingFileName).toURI();
+		final TextFile textFile = new TextFile(fileUri);
+
+		final String mappingFileSource = fileUri.getPath().replace(mappingFileName, "");
+
+		context.checking(new Expectations() {{
+			atLeast(1).of(mappingsFileSource).exists();
+			will(returnValue(true));
+			oneOf(mappingsFileSource).listFilesRecursively();
+			will(returnValue(Lists.newArrayList(textFile)));
+			oneOf(mappingsFileSource).getUri();
+			will(returnValue(URI.create(mappingFileSource)));
+		}});
+
+		jsonFileMappingsSource.loadMappingsInto(stubMappings);
+
+		final StubMapping stubMapping = stubMappings.getAll().get(0);
+		stubMapping.setTransient(true);
+
+		context.checking(new Expectations() {{
+			one(mappingsFileSource).writeTextFile(
+					with(mappingFileName),
+					with(Json.write(stubMapping))
+			);
+		}});
+
+		jsonFileMappingsSource.saveMappings(stubMappings);
+		assertThat(stubMapping.isTransient(), is(false));
+	}
+}
