013dcc87cf621ff67eacc3b59f5d49e59333dc76
Tom Akehurst <t.m.akehurst@googlemail.com>
2016-04-22 17:16:14 +0100

    add xpath value pattern

--- src/main/java/com/github/tomakehurst/wiremock/matching/EqualToXPathPattern.java
+++ src/main/java/com/github/tomakehurst/wiremock/matching/EqualToXPathPattern.java
@@ -1,6 +1,22 @@
 package com.github.tomakehurst.wiremock.matching;
 
+import org.custommonkey.xmlunit.NamespaceContext;
+import org.custommonkey.xmlunit.SimpleNamespaceContext;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.custommonkey.xmlunit.XpathEngine;
+import org.custommonkey.xmlunit.exceptions.XpathException;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static com.github.tomakehurst.wiremock.common.LocalNotifier.notifier;
+
 public class EqualToXPathPattern extends StringValuePattern {
+
+    private final Map<String, String> xpathNamespaces = null;
 
     public EqualToXPathPattern(String expectedValue) {
         super(expectedValue);
@@ -8,6 +24,29 @@ public class EqualToXPathPattern extends StringValuePattern {
 
     @Override
     public MatchResult match(String value) {
-        return null;
+        return MatchResult.of(isXPathMatch(value));
+    }
+
+    private boolean isXPathMatch(String value) {
+        try {
+            Document inDocument = XMLUnit.buildControlDocument(value);
+            XpathEngine simpleXpathEngine = XMLUnit.newXpathEngine();
+            if (xpathNamespaces != null) {
+                NamespaceContext namespaceContext = new SimpleNamespaceContext(xpathNamespaces);
+                simpleXpathEngine.setNamespaceContext(namespaceContext);
+            }
+            NodeList nodeList = simpleXpathEngine.getMatchingNodes(expectedValue, inDocument);
+            return nodeList.getLength() > 0;
+        } catch (SAXException e) {
+            notifier().info(String.format(
+                "Warning: failed to parse the XML document. Reason: %s\nXML: %s", e.getMessage(), value));
+            return false;
+        } catch (IOException e) {
+            notifier().info(e.getMessage());
+            return false;
+        } catch (XpathException e) {
+            notifier().info("Warning: failed to evaluate the XPath expression " + expectedValue);
+            return false;
+        }
     }
 }
--- src/test/java/com/github/tomakehurst/wiremock/matching/EqualToXPathPatternTest.java
+++ src/test/java/com/github/tomakehurst/wiremock/matching/EqualToXPathPatternTest.java
@@ -2,10 +2,63 @@ package com.github.tomakehurst.wiremock.matching;
 
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 public class EqualToXPathPatternTest {
 
     @Test
     public void returnsExactMatchWhenXPathMatches() {
+        String mySolarSystemXML = "<solar-system>"
+            + "<planet name='Earth' position='3' supportsLife='yes'/>"
+            + "<planet name='Venus' position='4'/></solar-system>";
 
+
+        StringValuePattern pattern = StringValuePattern.equalToXPath("//planet[@name='Earth']");
+
+        MatchResult match = pattern.match(mySolarSystemXML);
+        assertTrue("Expected XPath match", match.isExactMatch());
+        assertThat(match.getDistance(), is(0.0));
+    }
+
+    @Test
+    public void returnsNoExactMatchWhenXPathDoesNotMatch() {
+        String mySolarSystemXML = "<solar-system>"
+            + "<planet name='Earth' position='3' supportsLife='yes'/>"
+            + "<planet name='Venus' position='4'/></solar-system>";
+
+        StringValuePattern pattern = StringValuePattern.equalToXPath("//star[@name='alpha centauri']");
+
+        MatchResult match = pattern.match(mySolarSystemXML);
+        assertFalse("Expected XPath non-match", match.isExactMatch());
+        assertThat(match.getDistance(), is(1.0));
+    }
+
+    @Test
+    public void returnsNoExactMatchWhenXPathExpressionIsInvalid() {
+        String mySolarSystemXML = "<solar-system>"
+            + "<planet name='Earth' position='3' supportsLife='yes'/>"
+            + "<planet name='Venus' position='4'/></solar-system>";
+
+        StringValuePattern pattern = StringValuePattern.equalToXPath("//\\\\&&&&&");
+
+        MatchResult match = pattern.match(mySolarSystemXML);
+        assertFalse("Expected XPath non-match", match.isExactMatch());
+        assertThat(match.getDistance(), is(1.0));
+    }
+
+    @Test
+    public void returnsNoExactMatchWhenXmlIsBadlyFormed() {
+        String mySolarSystemXML = "solar-system>"
+            + "<planet name='Earth' position='3' supportsLife='yes'/>"
+            + "<planet name='Venus' position='4'/></solar-system>";
+
+        StringValuePattern pattern = StringValuePattern.equalToXPath("//star[@name='alpha centauri']");
+
+        MatchResult match = pattern.match(mySolarSystemXML);
+        assertFalse("Expected XPath non-match", match.isExactMatch());
+        assertThat(match.getDistance(), is(1.0));
     }
 }
