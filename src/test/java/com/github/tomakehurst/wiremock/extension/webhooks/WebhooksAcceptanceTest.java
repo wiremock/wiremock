44011c8d701ccd58f408b74ae9b087df8ca36e0c
Tom Akehurst <t.m.akehurst@googlemail.com>
2016-09-01 15:08:25 +0100

    Removed static state from webhooks test

--- src/test/java/com/github/tomakehurst/wiremock/extension/webhooks/WebhooksAcceptanceTest.java
+++ src/test/java/com/github/tomakehurst/wiremock/extension/webhooks/WebhooksAcceptanceTest.java
@@ -1,7 +1,5 @@
 package com.github.tomakehurst.wiremock.extension.webhooks;
 
-import com.github.tomakehurst.wiremock.AcceptanceTestBase;
-import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.http.Request;
 import com.github.tomakehurst.wiremock.http.RequestListener;
@@ -11,13 +9,14 @@ import com.github.tomakehurst.wiremock.testsupport.TestNotifier;
 import com.github.tomakehurst.wiremock.testsupport.WireMockTestClient;
 import org.apache.http.entity.StringEntity;
 import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 
 import java.util.concurrent.CountDownLatch;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.any;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static com.github.tomakehurst.wiremock.extension.webhooks.Webhooks.webhook;
 import static com.github.tomakehurst.wiremock.http.RequestMethod.GET;
@@ -27,50 +26,42 @@ import static org.apache.http.entity.ContentType.TEXT_PLAIN;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
-public class WebhooksAcceptanceTest extends AcceptanceTestBase {
+public class WebhooksAcceptanceTest {
 
-    static Webhooks webhooks = new Webhooks();
+    @Rule
+    public WireMockRule targetServer = new WireMockRule(options().dynamicPort());
 
-    static CountDownLatch latch;
-    static WireMockServer targetServer;
-    static WireMock wm;
-    static TestNotifier notifier = new TestNotifier();
-
+    CountDownLatch latch;
+    Webhooks webhooks = new Webhooks();
+    TestNotifier notifier = new TestNotifier();
     WireMockTestClient client;
 
-    @ClassRule
-    public static WireMockRule rule = new WireMockRule(
+    @Rule
+    public WireMockRule rule = new WireMockRule(
         options()
             .dynamicPort()
             .notifier(notifier)
             .extensions(webhooks));
 
 
-
-    @BeforeClass
-    public static void initClass() {
-        targetServer = wireMockServer;
-        wm = new WireMock(rule.port());
-
+    @Before
+    public void init() {
         targetServer.addMockServiceRequestListener(new RequestListener() {
             @Override
             public void requestReceived(Request request, Response response) {
                 latch.countDown();
             }
         });
-
-        System.out.println("Target server port: " + targetServer.port());
-        System.out.println("Under test server port: " + rule.port());
-    }
-
-    @Before
-    public void init() {
         reset();
         notifier.reset();
         targetServer.stubFor(any(anyUrl())
             .willReturn(aResponse().withStatus(200)));
         latch = new CountDownLatch(1);
         client = new WireMockTestClient(rule.port());
+        WireMock.configureFor(targetServer.port());
+
+        System.out.println("Target server port: " + targetServer.port());
+        System.out.println("Under test server port: " + rule.port());
     }
 
     @Test
@@ -79,7 +70,7 @@ public class WebhooksAcceptanceTest extends AcceptanceTestBase {
             .willReturn(aResponse().withStatus(200))
             .withPostServeAction("webhook", webhook()
                 .withMethod(POST)
-                .withUrl("http://localhost:" + wireMockServer.port() + "/callback")
+                .withUrl("http://localhost:" + targetServer.port() + "/callback")
                 .withHeader("Content-Type", "application/json")
                 .withBody("{ \"result\": \"SUCCESS\" }"))
         );
@@ -108,7 +99,7 @@ public class WebhooksAcceptanceTest extends AcceptanceTestBase {
             .willReturn(aResponse().withStatus(200))
             .withPostServeAction("webhook", webhook()
                 .withMethod(GET)
-                .withUrl("http://localhost:" + wireMockServer.port() + "/callback"))
+                .withUrl("http://localhost:" + targetServer.port() + "/callback"))
         );
 
         verify(0, postRequestedFor(anyUrl()));
