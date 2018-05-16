95e014cd265de253787b93f27595bf7b9d1bfd6b
Tom Akehurst <t.m.akehurst@googlemail.com>
2018-05-16 22:31:48 +0100

    Added a performance test sub-project

---
+++ perf-test/src/main/java/wiremock/LoadTestConfiguration.java
@@ -0,0 +1,165 @@
+package wiremock;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.common.Slf4jNotifier;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
+import com.github.tomakehurst.wiremock.global.GlobalSettings;
+import com.github.tomakehurst.wiremock.http.DelayDistribution;
+import com.github.tomakehurst.wiremock.http.UniformDistribution;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.apache.commons.lang3.RandomStringUtils.randomAscii;
+
+public class LoadTestConfiguration {
+
+    private WireMockServer wm;
+
+    public LoadTestConfiguration() {
+        wm = new WireMockServer(WireMockConfiguration.options()
+                .dynamicPort()
+                .dynamicHttpsPort()
+                .asynchronousResponseEnabled(true)
+                .asynchronousResponseThreads(50)
+                .containerThreads(50)
+                .maxRequestJournalEntries(1000)
+                .notifier(new Slf4jNotifier(false))
+                .extensions(new ResponseTemplateTransformer(false)));
+        wm.start();
+    }
+
+    public void before() {
+        // TODO: Optionally add delay
+        GlobalSettings globalSettings = new GlobalSettings();
+        globalSettings.setDelayDistribution(new UniformDistribution(100, 2000));
+        wm.updateGlobalSettings(globalSettings);
+
+        // Basic GET
+        for (int i = 1; i <= 50; i++) {
+            wm.stubFor(get("/load-test/" + i)
+                .withHeader("Accept", containing("text/plain"))
+                .willReturn(ok(randomAscii(1, 2000))));
+        }
+
+        // POST JSON equality
+        for (int i = 1; i <= 10; i++) {
+            wm.stubFor(post("/load-test/json")
+                .withHeader("Accept", equalTo("text/plain"))
+                .withHeader("Content-Type", matching(".*/json"))
+                .withRequestBody(equalToJson(POSTED_JSON))
+                .willReturn(ok(randomAscii(i * 200))));
+        }
+
+        // POST JSONPath
+        for (int i = 1; i <= 10; i++) {
+            wm.stubFor(post("/load-test/jsonpath")
+                .withHeader("Content-Type", equalTo("application/json"))
+                .withRequestBody(matchingJsonPath("$.matchThis.inner.innermost", equalTo("42")))
+                .willReturn(created()));
+        }
+
+        // POST XML equality
+        for (int i = 1; i <= 2; i++) {
+            wm.stubFor(post("/load-test/xml")
+                .withHeader("Content-Type", matching(".*/xml.*"))
+                .withRequestBody(equalToXml(POSTED_XML.replace("$1", String.valueOf(i))))
+                .willReturn(ok(randomAscii(i * 200))));
+        }
+
+        // POST XML XPath
+        for (int i = 1; i <= 10; i++) {
+            wm.stubFor(post("/load-test/xpath")
+                .withHeader("Content-Type", matching(".*/xml.*"))
+//                .withRequestBody(matchingXPath("//description[@subject = 'JWT']/text()", containing("JSON Web Token")))
+                .withRequestBody(matchingXPath("//description/text()", containing("JSON Web Token")))
+                .willReturn(ok(randomAscii(i * 200))));
+        }
+
+        // POST text body regex
+        for (int i = 1; i <= 10; i++) {
+            wm.stubFor(post("/load-test/text")
+                .withHeader("Content-Type", matching(".*text/plain.*"))
+                .withRequestBody(matching(".*[0-9]{5}.*"))
+                .willReturn(ok(randomAscii(i * 200))));
+        }
+
+        // TODO: Response templating
+        wm.stubFor(put("/load-test/templated")
+            .willReturn(ok(TEMPLATED_RESPONSE).withTransformers("response-template")));
+
+    }
+
+    public void after() {
+        wm.stop();
+    }
+
+    public int getWireMockPort() {
+        return wm.port();
+    }
+
+
+    public static final String POSTED_JSON = "{\n" +
+        "    \"things\": [\n" +
+        "        {\n" +
+        "            \"name\": \"First\",\n" +
+        "            \"value\": 111\n" +
+        "        },\n" +
+        "        {\n" +
+        "            \"name\": \"Second\",\n" +
+        "            \"value\": 22222\n" +
+        "        },\n" +
+        "        {\n" +
+        "            \"name\": \"Third\",\n" +
+        "            \"value\": 111\n" +
+        "        },\n" +
+        "        {\n" +
+        "            \"name\": \"Fourth\",\n" +
+        "            \"value\": 555\n" +
+        "        }\n" +
+        "    ],\n" +
+        "    \"meta\": {\n" +
+        "        \"countOfThings\": 4,\n" +
+        "        \"tags\": [\"one\", \"two\"]\n" +
+        "    }\n" +
+        "}";
+
+    public static final String JSON_FOR_JSON_PATH_MATCH = "{\n" +
+        "    \"matchThis\": {\n" +
+        "        \"inner\": {\n" +
+        "            \"innermost\": 42\n" +
+        "        }\n" +
+        "    }\n" +
+        "}";
+
+    public static final String POSTED_XML = "<?xml version=\"1.0\"?>\n" +
+            "\n" +
+            "<things id=\"$1\">\n" +
+            "    <stuff id=\"1\"/>\n" +
+            "    <fluff id=\"2\"/>\n" +
+            "\n" +
+            "    <inside>\n" +
+            "        <deep-inside level=\"3\">\n" +
+            "            <one/>\n" +
+            "            <two/>\n" +
+            "            <three/>\n" +
+            "            <four/>\n" +
+            "            <one/>\n" +
+            "            <description subject=\"JWT\">\n" +
+            "                JSON Web Token (JWT) is a compact, URL-safe means of representing claims to be transferred between two parties. The claims in a JWT are encoded as a JSON object that is used as the payload of a JSON Web Signature (JWS) structure or as the plaintext of a JSON Web Encryption (JWE) structure, enabling the claims to be digitally signed or integrity protected with a Message Authentication Code (MAC) and/or encrypted.\n" +
+            "            </description>\n" +
+            "        </deep-inside>\n" +
+            "    </inside>\n" +
+            "\n" +
+            "</things>";
+
+    public static final String TEMPLATED_RESPONSE = "Templated response\n" +
+            "==================\n" +
+            "\n" +
+            "{{date offset=\"-5 months\" format=\"yyyy-MM-dd\"}}\n" +
+            "\n" +
+            "{{date (parseDate request.headers.MyDate) timezone='Australia/Sydney'}}\n" +
+            "\n" +
+            "{{randomValue length=36 type='ALPHANUMERIC_AND_SYMBOLS'}}\n" +
+            "\n" +
+            "{{jsonPath request.body '$..inner'}}";
+}
