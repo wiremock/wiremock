48109cdd26ad2f0beb12c6e07128bb17e8a955b8
Tom Akehurst <t.m.akehurst@googlemail.com>
2018-06-24 08:49:50 +0100

    Added checking of 404s in the many GETs perf test scenario

--- perf-test/src/gatling/scala/wiremock/StubbingAndVerifyingSimulation.scala
+++ perf-test/src/gatling/scala/wiremock/StubbingAndVerifyingSimulation.scala
@@ -73,6 +73,10 @@ class StubbingAndVerifyingSimulation extends Simulation {
           .get(session => s"load-test/${random.nextInt(5999) + 1}")
           .header("Accept", "text/plain+stuff")
           .check(status.is(200)))
+        .exec(http("GETs")
+          .get(session => s"load-test/${random.nextInt(5999) + 6000}")
+          .header("Accept", "text/plain+stuff")
+          .check(status.is(404)))
       }
   }
 
