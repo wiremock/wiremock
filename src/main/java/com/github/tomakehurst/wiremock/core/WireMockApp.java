47efc08f67ae272bbe6c59cc4bc8c799ede5ec40
Tom Akehurst <tom@wiremock.org>
2019-07-02 09:28:28 +0100

    Added a before listener method for global settings, allowing changes to be vetoed

--- src/main/java/com/github/tomakehurst/wiremock/core/WireMockApp.java
+++ src/main/java/com/github/tomakehurst/wiremock/core/WireMockApp.java
@@ -392,10 +392,15 @@ public class WireMockApp implements StubServer, Admin {
     @Override
     public void updateGlobalSettings(GlobalSettings newSettings) {
         GlobalSettings oldSettings = globalSettingsHolder.get();
+
+        for (GlobalSettingsListener listener: globalSettingsListeners) {
+            listener.beforeGlobalSettingsUpdated(oldSettings, newSettings);
+        }
+
         globalSettingsHolder.replaceWith(newSettings);
 
         for (GlobalSettingsListener listener: globalSettingsListeners) {
-            listener.globalSettingsUpdated(oldSettings, newSettings);
+            listener.afterGlobalSettingsUpdated(oldSettings, newSettings);
         }
     }
 
