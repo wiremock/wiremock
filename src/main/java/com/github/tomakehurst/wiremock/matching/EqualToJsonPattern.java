fdb86638eef19bb56c38f2d281fe5349e091b273
Lukas Krecan <lukas.krecan@adleritech.net>
2019-12-10 19:46:08 +0100

    EqualToJsonPattern simplified

--- src/main/java/com/github/tomakehurst/wiremock/matching/EqualToJsonPattern.java
+++ src/main/java/com/github/tomakehurst/wiremock/matching/EqualToJsonPattern.java
@@ -6,25 +6,11 @@ import com.github.tomakehurst.wiremock.common.Json;
 import net.javacrumbs.jsonunit.core.Configuration;
 import net.javacrumbs.jsonunit.core.Option;
 import net.javacrumbs.jsonunit.core.internal.Diff;
-import net.javacrumbs.jsonunit.core.internal.Node;
 import net.javacrumbs.jsonunit.core.listener.Difference;
 import net.javacrumbs.jsonunit.core.listener.DifferenceContext;
 import net.javacrumbs.jsonunit.core.listener.DifferenceListener;
 
-import java.lang.reflect.Field;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-
-import static com.github.tomakehurst.wiremock.common.Exceptions.throwUnchecked;
-import static net.javacrumbs.jsonunit.core.internal.Node.NodeType.ARRAY;
-import static net.javacrumbs.jsonunit.core.internal.Node.NodeType.OBJECT;
-
 public class EqualToJsonPattern extends StringValuePattern {
-
-    static {
-        System.setProperty("json-unit.libraries", "jackson2");
-    }
 
     private final Boolean ignoreArrayOrder;
     private final Boolean ignoreExtraElements;
@@ -66,12 +52,14 @@ public class EqualToJsonPattern extends StringValuePattern {
             diffConfig = diffConfig.withOptions(Option.IGNORING_EXTRA_ARRAY_ITEMS, Option.IGNORING_EXTRA_FIELDS);
         }
 
+        final JsonNode expected = Json.read(expectedValue, JsonNode.class);
+        final JsonNode actual = Json.read(value, JsonNode.class);
+
         final Diff diff;
         try {
-
             diff = Diff.create(
-                    expectedValue,
-                    value,
+                    expected, // JsonUnit knows how to work with JsonNode
+                    actual,
                     "",
                     "",
                     diffConfig
@@ -89,8 +77,6 @@ public class EqualToJsonPattern extends StringValuePattern {
             @Override
             public double getDistance() {
                 diff.similar();
-                Node expected = getNodeFromDiff("expectedRoot", diff);
-                Node actual = getNodeFromDiff("actualRoot", diff);
                 double maxNodes = maxDeepSize(expected, actual);
                 return diffListener.count / maxNodes;
             }
@@ -142,71 +128,8 @@ public class EqualToJsonPattern extends StringValuePattern {
     }
 
     private static int deepSize(Object nodeObj) {
-        if (nodeObj == null) {
-            return 0;
-        }
-
-        if (nodeObj instanceof String && ((String) nodeObj).isEmpty()) {
-            return 0;
-        }
-
-        if (!(nodeObj instanceof Node)) {
-            return 1;
-        }
-
-        Node node = (Node) nodeObj;
-
-        int acc = 1;
-        if (isContainerNode(nodeObj)) {
-            for (Object child: allValues(node)) {
-                acc++;
-                if (isContainerNode(child)) {
-                    acc += deepSize(child);
-                }
-            }
-        }
-
-        return acc;
-    }
-
-    private static boolean isContainerNode(Object nodeObj) {
-        if (!(nodeObj instanceof Node)) {
-            return false;
-        }
-
-        Node node = (Node) nodeObj;
-        final Node.NodeType nodeType = node.getNodeType();
-        return nodeType == ARRAY || nodeType == OBJECT;
-    }
-
-    private static Iterable<Object> allValues(Node containerNode) {
-        final Node.NodeType nodeType = containerNode.getNodeType();
-        if (nodeType == ARRAY) {
-            return toNodeList(containerNode);
-        } else if (nodeType == OBJECT) {
-            return toNodeMap(containerNode).values();
-        }
-
-        return Collections.<Object>singletonList(containerNode);
-    }
-
-    @SuppressWarnings("unchecked")
-    private static List<Object> toNodeList(Node node) {
-        return ((List<Object>) ARRAY.getValue(node));
-    }
-
-    @SuppressWarnings("unchecked")
-    private static Map<String, Object> toNodeMap(Node node) {
-        return ((Map<String, Object>) OBJECT.getValue(node));
-    }
-
-    private static Node getNodeFromDiff(String fieldName, Diff diff) {
-        try {
-            final Field field = Diff.class.getDeclaredField(fieldName);
-            field.setAccessible(true);
-            return (Node) field.get(diff);
-        } catch (Exception e) {
-            return throwUnchecked(e, Node.class);
-        }
+        // Jackson knows how to convert object returned by Difference.get???() to JsonNode
+        JsonNode jsonNode = Json.getObjectMapper().convertValue(nodeObj, JsonNode.class);
+        return Json.deepSize(jsonNode);
     }
 }
