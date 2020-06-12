524456e7826bdeaff2637ca1d61ed325f51608fc
Robert Elliot <rob@lidalia.org.uk>
2020-06-12 12:08:00 +0100

    Simplify array prepending and exception handling

--- src/test/java/com/github/tomakehurst/wiremock/common/ArrayFunctionsTest.java
+++ src/test/java/com/github/tomakehurst/wiremock/common/ArrayFunctionsTest.java
@@ -3,6 +3,7 @@ package com.github.tomakehurst.wiremock.common;
 import org.junit.Test;
 
 import static com.github.tomakehurst.wiremock.common.ArrayFunctions.concat;
+import static com.github.tomakehurst.wiremock.common.ArrayFunctions.prepend;
 import static org.junit.Assert.*;
 
 public class ArrayFunctionsTest {
@@ -47,5 +48,38 @@ public class ArrayFunctionsTest {
         first[0] = 10;
         second[0] = 30;
         assertArrayEquals(new Integer[] { 1, 2, 3, 4 }, result);
+    }
+
+    @Test
+    public void prependNullAndEmpty() {
+        assertArrayEquals(new Integer[] { null }, prepend(null, empty));
+    }
+
+    @Test
+    public void prependSomeAndEmpty() {
+        Integer[] result = prepend(1, empty);
+        assertArrayEquals(new Integer[] { 1 }, result);
+    }
+
+    @Test
+    public void prependNullAndNonEmpty() {
+        Integer[] second = {1, 2};
+
+        Integer[] result = prepend(null, second);
+        assertArrayEquals(new Integer[] { null, 1, 2 }, result);
+
+        second[0] = 10;
+        assertArrayEquals(new Integer[] { null, 1, 2 }, result);
+    }
+
+    @Test
+    public void prependSomeAndNonEmpty() {
+        Integer[] second = {2, 3};
+
+        Integer[] result = prepend(1, second);
+        assertArrayEquals(new Integer[] { 1, 2, 3 }, result);
+
+        second[0] = 30;
+        assertArrayEquals(new Integer[] { 1, 2, 3 }, result);
     }
 }
