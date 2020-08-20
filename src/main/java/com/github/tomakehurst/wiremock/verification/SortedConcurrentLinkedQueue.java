effee7c4bc41832cffbe9f375baea07756892c9f
yvadapal <Happylife@95>
2020-08-20 13:32:10 -0500

    refactoring code

--- src/main/java/com/github/tomakehurst/wiremock/verification/SortedConcurrentLinkedQueue.java
+++ src/main/java/com/github/tomakehurst/wiremock/verification/SortedConcurrentLinkedQueue.java
@@ -1,17 +1,10 @@
 package com.github.tomakehurst.wiremock.verification;
 
 import com.github.tomakehurst.wiremock.stubbing.ServeEvent;
-import com.google.common.base.Function;
-import com.google.common.base.Optional;
-import com.google.common.base.Predicate;
-import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
-
-
-import static com.google.common.collect.Iterables.*;
 
 public class SortedConcurrentLinkedQueue extends PersistJournalRequestWrapper {
 
@@ -27,11 +20,7 @@ public class SortedConcurrentLinkedQueue extends PersistJournalRequestWrapper {
 	}
 
 	@Override
-	public List<ServeEvent> remove(Predicate<ServeEvent> predicate) {
-
-		List<ServeEvent> toDelete = FluentIterable.from(serveEvents)
-				.filter(predicate)
-				.toList();
+	public List<ServeEvent> remove(List<ServeEvent> toDelete) {
 
 		for (ServeEvent event: toDelete) {
 			serveEvents.remove(event);
@@ -46,13 +35,8 @@ public class SortedConcurrentLinkedQueue extends PersistJournalRequestWrapper {
 	}
 
 	@Override
-	public Optional<ServeEvent> getServeEvent(final UUID id) {
-		return tryFind(serveEvents, new Predicate<ServeEvent>() {
-			@Override
-			public boolean apply(ServeEvent input) {
-				return input.getId().equals(id);
-			}
-		});
+	protected Queue<ServeEvent> getServeQueue() {
+		return serveEvents;
 	}
 
 	@Override
@@ -60,22 +44,4 @@ public class SortedConcurrentLinkedQueue extends PersistJournalRequestWrapper {
 		return ImmutableList.copyOf(serveEvents).reverse();
 	}
 
-	@Override
-	protected Iterable<LoggedRequest> getRequests() {
-
-		return transform(serveEvents, new Function<ServeEvent, LoggedRequest>() {
-			public LoggedRequest apply(ServeEvent input) {
-				return input.getRequest();
-			}
-		});
-	}
-
-	@Override
-	protected void removeOldEntries(Optional<Integer> maxEntries) {
-		if (maxEntries.isPresent()) {
-			while (serveEvents.size() > maxEntries.get()) {
-				serveEvents.poll();
-			}
-		}
-	}
 }
