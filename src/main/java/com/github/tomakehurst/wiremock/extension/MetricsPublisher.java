6233432bf43ce5213e58211d67b266a4fda75ea3
nitinPaypay <58462241+nitinPaypay@users.noreply.github.com>
2020-10-15 17:41:57 +0900

    Generalized MetricsPublisher

--- src/main/java/com/github/tomakehurst/wiremock/extension/MetricsPublisher.java
+++ src/main/java/com/github/tomakehurst/wiremock/extension/MetricsPublisher.java
@@ -1,6 +1,14 @@
 package com.github.tomakehurst.wiremock.extension;
 
 
+import com.sun.istack.internal.Nullable;
+
+import java.util.function.ToDoubleFunction;
+
 public interface MetricsPublisher extends Extension {
   boolean summary(String metricName, double metricData, String... tags);
+
+  boolean counter(String name, String... tags);
+
+  <T> T gauge(String name, @Nullable T stateObject, ToDoubleFunction<T> valueFunction, String... tags);
 }
