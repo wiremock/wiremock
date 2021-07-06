001c9504248243d4b504e9e6dccb767f80ba66d8
Tom Akehurst <t.m.akehurst@gmail.com>
2021-07-06 18:28:07 +0100

    randomDecimal helper now accepts integer and string bounds in addition to decimal

--- src/main/java/com/github/tomakehurst/wiremock/extension/responsetemplating/helpers/RandomDecimalHelper.java
+++ src/main/java/com/github/tomakehurst/wiremock/extension/responsetemplating/helpers/RandomDecimalHelper.java
@@ -21,14 +21,15 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.concurrent.ThreadLocalRandom;
 
+import static com.github.tomakehurst.wiremock.extension.responsetemplating.helpers.HelperUtils.coerceToDouble;
 import static java.math.BigDecimal.ROUND_HALF_UP;
 
 public class RandomDecimalHelper extends HandlebarsHelper<Void> {
 
     @Override
     public Object apply(Void context, Options options) throws IOException {
-        double lowerBound = options.hash("lower", Double.MIN_VALUE);
-        double upperBound = options.hash("upper", Double.MAX_VALUE);
+        double lowerBound = coerceToDouble(options.hash("lower", Double.MIN_VALUE));
+        double upperBound = coerceToDouble(options.hash("upper", Double.MAX_VALUE));
 
         return ThreadLocalRandom.current().nextDouble(lowerBound, upperBound);
     }
