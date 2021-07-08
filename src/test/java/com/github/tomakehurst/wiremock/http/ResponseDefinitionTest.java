38baf9a6df5da1eb71c6a6bf1b9a6c14503c7f60
Sathya Ghelani <sathyaghelani@gmail.com>
2021-07-08 20:20:17 +0100

    added tests for retrieving proxy url

---
+++ src/test/java/com/github/tomakehurst/wiremock/http/ResponseDefinitionTest.java
@@ -0,0 +1,30 @@
+package com.github.tomakehurst.wiremock.http;
+
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
+import com.github.tomakehurst.wiremock.matching.MockRequest;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ResponseDefinitionTest {
+
+    @Test
+    public void getProxyUrlGivesBackRequestUrlIfBrowserProxyRequest() {
+        ResponseDefinition response = ResponseDefinition.browserProxy(MockRequest.mockRequest()
+                .host("http://my.domain").url("/path").isBrowserProxyRequest(true));
+
+        assertThat(response.getProxyUrl(), equalTo("http://my.domain/path"));
+    }
+
+    @Test
+    public void getProxyUrlGivesBackTheProxyUrlWhenNotBrowserProxy() {
+        ResponseDefinition response = ResponseDefinitionBuilder.responseDefinition()
+                .proxiedFrom("http://my.proxy.url")
+                .build();
+
+        response.setOriginalRequest(MockRequest.mockRequest().url("/path"));
+
+        assertThat(response.getProxyUrl(), equalTo("http://my.proxy.url/path"));
+    }
+}
