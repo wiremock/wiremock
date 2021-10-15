28665717a9cfb1e585c1c01e1d83dbada7e1a385
Tim te Beek <tim.te.beek@jdriven.com>
2021-10-15 20:35:51 +0200

    Convert webhooks tests to JUnit Jupiter

--- wiremock-webhooks-extension/src/test/java/functional/FailingWebhookTest.java
+++ wiremock-webhooks-extension/src/test/java/functional/FailingWebhookTest.java
@@ -1,86 +1,85 @@
 package functional;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.any;
-import static com.github.tomakehurst.wiremock.client.WireMock.anyUrl;
-import static com.github.tomakehurst.wiremock.client.WireMock.post;
-import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.reset;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.verify;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
-import static com.github.tomakehurst.wiremock.http.RequestMethod.POST;
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.apache.http.entity.ContentType.TEXT_PLAIN;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-import static org.wiremock.webhooks.Webhooks.webhook;
-
 import com.github.tomakehurst.wiremock.client.WireMock;
-import com.github.tomakehurst.wiremock.http.Request;
-import com.github.tomakehurst.wiremock.http.RequestListener;
-import com.github.tomakehurst.wiremock.http.Response;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
-import java.util.concurrent.CountDownLatch;
+import com.github.tomakehurst.wiremock.core.Admin;
+import com.github.tomakehurst.wiremock.extension.PostServeAction;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import com.github.tomakehurst.wiremock.stubbing.ServeEvent;
 import org.apache.http.entity.StringEntity;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.wiremock.webhooks.Webhooks;
 import testsupport.TestNotifier;
 import testsupport.ThrowingWebhookTransformer;
 import testsupport.WireMockTestClient;
 
+import java.util.concurrent.CountDownLatch;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static com.github.tomakehurst.wiremock.http.RequestMethod.POST;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.apache.http.entity.ContentType.TEXT_PLAIN;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.wiremock.webhooks.Webhooks.webhook;
+
 public class FailingWebhookTest {
 
-  @Rule
-  public WireMockRule targetServer = new WireMockRule(options().dynamicPort());
+  @RegisterExtension
+  public WireMockExtension targetServer = WireMockExtension.newInstance()
+    .options(options().dynamicPort()
+      .extensions(new PostServeAction() {
+        @Override
+        public void doGlobalAction(ServeEvent serveEvent, Admin admin) {
+          if (serveEvent.getRequest().getUrl().startsWith("/callback")) {
+            latch.countDown();
+          }
+        }
+
+        @Override
+        public String getName() {
+          return "test-latch";
+        }
+      }))
+    .build();
 
   CountDownLatch latch;
 
-  Webhooks webhooks = new Webhooks(
-      new ThrowingWebhookTransformer()
-  );
+  Webhooks webhooks = new Webhooks(new ThrowingWebhookTransformer());
 
   TestNotifier notifier = new TestNotifier();
   WireMockTestClient client;
 
-  @Rule
-  public WireMockRule rule = new WireMockRule(
-      options()
-          .dynamicPort()
-          .notifier(notifier)
-          .extensions(webhooks));
+  @RegisterExtension
+  public WireMockExtension extension = WireMockExtension.newInstance()
+    .configureStaticDsl(true)
+    .options(options()
+      .dynamicPort()
+      .notifier(notifier)
+      .extensions(webhooks))
+    .build();
 
-  @Before
+  @BeforeEach
   public void init() {
-    targetServer.addMockServiceRequestListener(new RequestListener() {
-      @Override
-      public void requestReceived(Request request, Response response) {
-        if (request.getUrl().startsWith("/callback")) {
-          latch.countDown();
-        }
-      }
-    });
-    reset();
     notifier.reset();
     targetServer.stubFor(any(anyUrl())
         .willReturn(aResponse().withStatus(200)));
     latch = new CountDownLatch(1);
-    client = new WireMockTestClient(rule.port());
-    WireMock.configureFor(targetServer.port());
+    client = new WireMockTestClient(extension.getRuntimeInfo().getHttpPort());
+    WireMock.configureFor(targetServer.getRuntimeInfo().getHttpPort());
   }
 
   @Test
   public void failWhenExecutingTheWebhook() throws Exception {
-    rule.stubFor(post(urlPathEqualTo("/something-async"))
+    extension.stubFor(post(urlPathEqualTo("/something-async"))
         .willReturn(aResponse().withStatus(200))
         .withPostServeAction("webhook", webhook()
             .withMethod(POST)
-            .withUrl("http://localhost:" + targetServer.port() + "/callback")
+            .withUrl(targetServer.url("/callback"))
             .withHeader("Content-Type", "application/json")
-            .withBody("{ \"result\": \"SUCCESS\" }"))
-    );
+            .withBody("{ \"result\": \"SUCCESS\" }")));
 
     verify(0, postRequestedFor(anyUrl()));
 
