073ecd161d1c3e640c9e5f42af4e0a0a8f49a23f
Jamie Tanna <jamie@jamietanna.co.uk>
2021-10-19 15:26:20 +0100

    Return an Bad Request when invalid `since` parameter is provided (#1623)
    As noted in #1595, we currently cause an Internal Server Error when an
    invalid date is provided to the admin `/requests` API.
    
    Instead of using the runtime exception `IllegalArgumentException`, which
    is less nice to try-catch, we can take advantage of the
    `InvalidInputException`, containing a `validation` error, which can be
    returned nicely as a JSON response.
    
    As this is using a JUnit4 test, we need to make sure that ArchUnit
    allows it as a valid use case, until `AcceptanceTestBase` is migrated to
    JUnit Jupiter.
    
    Closes #1595.

--- src/main/java/com/github/tomakehurst/wiremock/admin/Conversions.java
+++ src/main/java/com/github/tomakehurst/wiremock/admin/Conversions.java
@@ -16,6 +16,8 @@
 package com.github.tomakehurst.wiremock.admin;
 
 import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
+import com.github.tomakehurst.wiremock.common.Errors;
+import com.github.tomakehurst.wiremock.common.InvalidInputException;
 import com.github.tomakehurst.wiremock.http.QueryParameter;
 
 import java.text.ParseException;
@@ -35,7 +37,7 @@ public class Conversions {
                 new ISO8601DateFormat().parse(parameter.firstValue()) :
                 null;
         } catch (ParseException e) {
-            throw new IllegalArgumentException(parameter.firstValue() + " is not a valid ISO8601 date");
+            throw new InvalidInputException(Errors.validation(parameter.key(), parameter.firstValue() + " is not a valid ISO8601 date"));
         }
     }
 }
