488186075d2ee1f98779ea4ac193e217d769e39c
Tim te Beek <tim.te.beek@jdriven.com>
2021-11-19 23:03:53 +0100

    Remove now unused private method

--- src/main/java/com/github/tomakehurst/wiremock/common/xml/Xml.java
+++ src/main/java/com/github/tomakehurst/wiremock/common/xml/Xml.java
@@ -125,19 +125,6 @@ public class Xml {
     }
   }
 
-  private static String render(Node node) {
-    try {
-      StringWriter sw = new StringWriter();
-      Transformer transformer = TransformerFactory.newInstance().newTransformer();
-      transformer.setOutputProperty(OMIT_XML_DECLARATION, "yes");
-      transformer.setOutputProperty(INDENT, "yes");
-      transformer.transform(new DOMSource(node), new StreamResult(sw));
-      return sw.toString();
-    } catch (TransformerException e) {
-      return throwUnchecked(e, String.class);
-    }
-  }
-
   public static XmlDocument parse(String xml) {
     try {
       InputSource source = new InputSource(new StringReader(xml));
