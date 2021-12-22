8b5017423980e3df563b55fa9dc4f699336f92f4
Paul Poulain <paul.poulain1@vodafone.com>
2021-12-22 13:45:34 +0000

    Format with spotless to align with upstream changes to make resolving conflicts easier.

--- src/main/java/com/github/tomakehurst/wiremock/http/DelayDistribution.java
+++ src/main/java/com/github/tomakehurst/wiremock/http/DelayDistribution.java
@@ -21,24 +21,20 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 /**
  * Distribution that models delays.
  *
- * Implementations should be thread safe.
+ * <p>Implementations should be thread safe.
  */
-@JsonTypeInfo(
-        use = JsonTypeInfo.Id.NAME,
-        include = JsonTypeInfo.As.PROPERTY,
-        property = "type"
-)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 @JsonSubTypes({
-        @JsonSubTypes.Type(value = LogNormal.class, name = "lognormal"),
-        @JsonSubTypes.Type(value = UniformDistribution.class, name = "uniform"),
-        @JsonSubTypes.Type(value = FixedDelayDistribution.class, name = "fixed"),
-        @JsonSubTypes.Type(value = CappedLogNormal.class, name = "cappedlognormal")
+  @JsonSubTypes.Type(value = LogNormal.class, name = "lognormal"),
+  @JsonSubTypes.Type(value = UniformDistribution.class, name = "uniform"),
+  @JsonSubTypes.Type(value = FixedDelayDistribution.class, name = "fixed"),
+  @JsonSubTypes.Type(value = CappedLogNormal.class, name = "cappedlognormal")
 })
 public interface DelayDistribution {
-    /**
-     * Samples a delay in milliseconds from the distribution.
-     *
-     * @return next delay in millis
-     */
-    long sampleMillis();
+  /**
+   * Samples a delay in milliseconds from the distribution.
+   *
+   * @return next delay in millis
+   */
+  long sampleMillis();
 }
