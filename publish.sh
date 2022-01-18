7929b5a86765e98a53a678e762c2786678147baf
Tom Akehurst <t.m.akehurst@gmail.com>
2022-01-18 12:47:59 +0000

    Updated docs

---
+++ publish.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+bundle exec jekyll build
+cp -rf _site/* ../wiremock-gh-pages/
+
+pushd ../wiremock-gh-pages
+git add --all
+git commit -m "Updated docs"
+git push origin gh-pages
+popd
