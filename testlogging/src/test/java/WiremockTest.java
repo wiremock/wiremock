9d9cc52c277afd353c2902b007b0d5d1b405c567
Tim te Beek <tim.te.beek@jdriven.com>
2022-01-27 22:29:07 +0100

    Fix incorrect identation

--- testlogging/src/test/java/WiremockTest.java
+++ testlogging/src/test/java/WiremockTest.java
@@ -43,7 +43,7 @@ public class WiremockTest {
         URL uri = new URL("http://localhost:8080/blah");
         InputStream content = uri.openConnection().getInputStream();
 
-         String retrievedBody = IOUtils.toString(content);
+        String retrievedBody = IOUtils.toString(content);
         assertEquals("body", retrievedBody);
         assertThat(stdOutCapture.toString(), containsString("c.g.t.wiremock.common.Log4jNotifier - Received request to /mappings/new"));
     }
