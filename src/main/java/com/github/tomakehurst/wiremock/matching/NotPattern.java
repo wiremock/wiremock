dd9a6f4f631eb531366bb09ec36004a2e9ee6543
Arihant Kaushik <42761135+SatyamAK@users.noreply.github.com>
2023-03-14 02:24:16 +0530

    New Feature: not() in verify #1892 (#2006)
    * [Arihant|Manisha] Add. not pattern matching functionality
    
    * [Manisha|Arihant] Add. deserialize not path pattern is wip
    
    * [Arihant|Manisha] Refactor. deserialize not path pattern
    
    * [Manisha|Arihant] Refactor. verifies withBody does not contain value
    
    * [Manisha|Arihant] Refactor. verifies withHeader does not contain value
    
    * [Manisha|Arihant] Refactor. verifies withHeader does not contain values
    
    * [Arihant|Manisha] Refactor. getdistance logic in notpattern class
    
    ---------
    
    Co-authored-by: Manisha Agarwal <manishaagarwal@Manishas-MacBook-Pro.local>
    Co-authored-by: manishaag7 <manishaagarwal1607@gmail.com>

---
+++ src/main/java/com/github/tomakehurst/wiremock/matching/NotPattern.java
@@ -0,0 +1,41 @@
+package com.github.tomakehurst.wiremock.matching;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class NotPattern extends StringValuePattern{
+
+    private StringValuePattern unexpectedPattern;
+
+    public NotPattern(@JsonProperty("not") StringValuePattern unexpectedPattern) {
+        super(unexpectedPattern.expectedValue);
+        this.unexpectedPattern = unexpectedPattern;
+    }
+
+    public StringValuePattern getNot(){
+        return unexpectedPattern;
+    }
+
+    @Override
+    public MatchResult match(String value) {
+        return invert(unexpectedPattern.match(value));
+    }
+
+    private MatchResult invert(final MatchResult matchResult){
+        return new MatchResult() {
+            @Override
+            public boolean isExactMatch() {
+                return !matchResult.isExactMatch();
+            }
+
+            @Override
+            public double getDistance() {
+                if(matchResult.isExactMatch()){
+                    return 0;
+                }
+                return 1.0;
+            }
+        };
+    }
+}
