6bb45ebdc786937896281a711e8bac377235bf03
kapishmalik <kapish15026@iiitd.ac.in>
2023-03-13 16:59:46 +0530

    Support disabling of browser proxy pass-through (#2087)
    * flag setting for browser proxy pass through
    
    * add setting for browser proxy pass through
    
    * pass proxypassthrough as option
    
    * pass proxypassthrough as option on programmatic startup
    
    * incorporated review comment change
    
    * incorporated one review comment change - com.github.tomakehurst.wiremock.global.GlobalSettings.Builder#proxyPassThrough variable type to primitive
    
    * incorporated remaining review comment change
    
    * replacing with primitive type in getter method as well
    
    * review comment changes

--- src/main/java/com/github/tomakehurst/wiremock/http/StubResponseRenderer.java
+++ src/main/java/com/github/tomakehurst/wiremock/http/StubResponseRenderer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2022 Thomas Akehurst
+ * Copyright (C) 2011-2023 Thomas Akehurst
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -67,7 +67,9 @@ public class StubResponseRenderer implements ResponseRenderer {
 
   private Response buildResponse(ServeEvent serveEvent) {
     if (serveEvent.getResponseDefinition().isProxyResponse()) {
-      return proxyResponseRenderer.render(serveEvent);
+      return settingsStore.get().getProxyPassThrough()
+          ? proxyResponseRenderer.render(serveEvent)
+          : Response.notConfigured();
     } else {
       Response.Builder responseBuilder = renderDirectly(serveEvent);
       return responseBuilder.build();
