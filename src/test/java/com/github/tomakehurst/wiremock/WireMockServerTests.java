d7216623e163d2a043338beb678aa279e94a9e90
Yehor Kovalchuk <developeregor96@gmail.com>
2023-04-24 15:38:59 +0000

    Add support filename template (#2136)
    * Add support filename template
    
    * Fix for formatting
    
    * Change logic for templating filename. Change default template. Decorate sanitize logic
    
    * Fix for detected format issues
    
    * Code cleanup
    
    * Fix for typo
    
    * Add new filename template Wiremock option. Delete SafeName util class and replace with FilenameMaker
    
    * Test cleanup. Check github life
    
    * Add FilenameMaker unit test
    
    * Fix for code formatting
    
    * Added failing tests for review points
    
    * Improved test method name for filename maker
    
    * Add checks for name inside stub. Replace url on urlPath in case of stub contains path
    
    * Stub name is now used in generated filenames via templating rather than prefixed. Wrapped the stub mapping into a model used for filename templates in order to normalise the URL and simplify access to common attributes.
    
    * Added a test case for generating a filename from an all-defaults stub mapping
    
    ---------
    
    Co-authored-by: Yehor Kovalchuk <yehor@wiremock.io>
    Co-authored-by: Tom Akehurst <t.m.akehurst@gmail.com>

--- src/test/java/com/github/tomakehurst/wiremock/WireMockServerTests.java
+++ src/test/java/com/github/tomakehurst/wiremock/WireMockServerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013-2021 Thomas Akehurst
+ * Copyright (C) 2013-2023 Thomas Akehurst
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -57,6 +57,14 @@ public class WireMockServerTests {
   public void returnsOptionsWhenCallingGetOptions() {
     Options options = new WireMockConfiguration();
     WireMockServer wireMockServer = new WireMockServer(options);
+    assertThat(wireMockServer.getOptions(), is(options));
+  }
+
+  @Test
+  public void addFilenameTemplateAsOptionAndValidFormat() {
+    Options options = options().filenameTemplate("{{{request.url}}}-{{{request.url}}}.json");
+    WireMockServer wireMockServer = new WireMockServer(options);
+    wireMockServer.start();
     assertThat(wireMockServer.getOptions(), is(options));
   }
 
