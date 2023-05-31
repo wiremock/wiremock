6f89182ac3fe5b8c6e2b342df9a2bf96d81fc048
Kirill Peshin <kirill.peshin@bsc-ideas.com>
2023-05-31 19:04:11 +0300

    Replace com.google.common.base.Function

--- src/main/java/com/github/tomakehurst/wiremock/recording/ScenarioProcessor.java
+++ src/main/java/com/github/tomakehurst/wiremock/recording/ScenarioProcessor.java
@@ -19,8 +19,6 @@ import com.github.tomakehurst.wiremock.common.Urls;
 import com.github.tomakehurst.wiremock.matching.RequestPattern;
 import com.github.tomakehurst.wiremock.stubbing.Scenario;
 import com.github.tomakehurst.wiremock.stubbing.StubMapping;
-import com.google.common.base.Function;
-import com.google.common.base.Predicate;
 import com.google.common.collect.*;
 import java.net.URI;
 import java.util.Collection;
@@ -33,22 +31,12 @@ public class ScenarioProcessor {
     ImmutableListMultimap<RequestPattern, StubMapping> stubsGroupedByRequest =
         Multimaps.index(
             stubMappings,
-            new Function<StubMapping, RequestPattern>() {
-              @Override
-              public RequestPattern apply(StubMapping mapping) {
-                return mapping.getRequest();
-              }
-            });
+            StubMapping::getRequest);
 
     Map<RequestPattern, Collection<StubMapping>> groupsWithMoreThanOneStub =
         Maps.filterEntries(
             stubsGroupedByRequest.asMap(),
-            new Predicate<Map.Entry<RequestPattern, Collection<StubMapping>>>() {
-              @Override
-              public boolean apply(Map.Entry<RequestPattern, Collection<StubMapping>> input) {
-                return input.getValue().size() > 1;
-              }
-            });
+            input -> input.getValue().size() > 1);
 
     int scenarioIndex = 0;
     for (Map.Entry<RequestPattern, Collection<StubMapping>> entry :
@@ -62,7 +50,7 @@ public class ScenarioProcessor {
     StubMapping firstScenario = stubMappings.get(0);
     String scenarioName =
         "scenario-"
-            + Integer.toString(scenarioIndex)
+            + scenarioIndex
             + "-"
             + Urls.urlToPathParts(URI.create(firstScenario.getRequest().getUrl()));
 
