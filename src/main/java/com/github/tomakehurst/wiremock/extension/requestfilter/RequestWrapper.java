58f586526e68039e3adce55bd52f81074944b892
Tom Akehurst <t.m.akehurst@googlemail.com>
2023-05-31 11:31:15 +0100

    Remove redundant guava imports

--- src/main/java/com/github/tomakehurst/wiremock/extension/requestfilter/RequestWrapper.java
+++ src/main/java/com/github/tomakehurst/wiremock/extension/requestfilter/RequestWrapper.java
@@ -23,8 +23,6 @@ import static org.apache.commons.lang3.StringUtils.countMatches;
 import static org.apache.commons.lang3.StringUtils.ordinalIndexOf;
 
 import com.github.tomakehurst.wiremock.http.*;
-import com.google.common.base.Function;
-import com.google.common.base.Predicate;
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableMap;
 import java.util.*;
