37976a07a6efb92f858fa5fa1e701a90b95adf80
Mark Henry <markhenry.software@gmail.com>
2023-05-31 04:34:18 -0700

    Configure default ObjectMapper with JavaTimeModule (#2130)
    * Add test asserting LocalDates can be used as parameters
    
    * Add support for LocalDate parameters

--- src/main/java/com/github/tomakehurst/wiremock/common/Json.java
+++ src/main/java/com/github/tomakehurst/wiremock/common/Json.java
@@ -26,6 +26,8 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.io.IOException;
 import java.util.Map;
 
@@ -46,6 +48,8 @@ public final class Json {
           objectMapper.configure(JsonParser.Feature.IGNORE_UNDEFINED, true);
           objectMapper.configure(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN, true);
           objectMapper.configure(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS, true);
+          objectMapper.registerModule(new JavaTimeModule());
+          objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
           return objectMapper;
         }
       };
