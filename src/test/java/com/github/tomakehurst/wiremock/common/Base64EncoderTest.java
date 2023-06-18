a80e2f6e27009db7d74b9bc7ecb6c7aa661f7636
Kirill Peshin <kirill.peshin@bscmsc.ru>
2023-06-18 12:36:38 +0300

    Replace com.google.common.io.BaseEncoding by java.util.Base64

--- src/test/java/com/github/tomakehurst/wiremock/common/Base64EncoderTest.java
+++ src/test/java/com/github/tomakehurst/wiremock/common/Base64EncoderTest.java
@@ -25,8 +25,8 @@ public class Base64EncoderTest {
   public static final String OUTPUT = "MTIzNA==";
 
   @Test
-  public void testGuavaEncoder() {
-    Base64Encoder encoder = new JdkBase64Encoder();
+  void testEncoder() {
+    var encoder = new JdkBase64Encoder();
 
     String encoded = encoder.encode(INPUT.getBytes());
     assertThat(encoded, is(OUTPUT));
