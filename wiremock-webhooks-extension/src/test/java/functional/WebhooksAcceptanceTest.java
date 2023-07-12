03b09e40bd657a89287589356e4ec5fab3b7597b
Warren Crossing <warren@itcl.com.au>
2023-07-12 22:07:17 +1000

    separate bodyfilename and body string out to two different tests

--- wiremock-webhooks-extension/src/test/java/functional/WebhooksAcceptanceTest.java
+++ wiremock-webhooks-extension/src/test/java/functional/WebhooksAcceptanceTest.java
@@ -202,22 +202,15 @@ public class WebhooksAcceptanceTest {
     verify(postRequestedFor(urlPathEqualTo("/callback2")));
   }
 
-  private WebhookDefinition withBodyOrBodyFileName(String bodyFileName, WebhookDefinition webhook) {
-      if (!bodyFileName.isEmpty()) { 
-          webhook.withBodyFileName(bodyFileName);
-      }
-      return webhook;
-  }
-
-  @ParameterizedTest
-  @ValueSource(strings = {"", "myFile.json"})
-  public void appliesTemplatingToUrlMethodHeadersAndBodyViaDSL(String bodyFileNameParam) throws Exception {
+  public void appliesTemplatingToUrlMethodHeadersAndBodyFileNameViaDSL() throws Exception {
     rule.stubFor(
         post(urlPathEqualTo("/templating"))
             .willReturn(ok())
             .withPostServeAction(
                 "webhook",
-                withBodyOrBodyFileName(bodyFileNameParam, webhook()
+
+                webhook()
+                    .withBodyFileName("myFile.json")
                     .withMethod("{{jsonPath originalRequest.body '$.method'}}")
                     .withUrl(
                         targetServer.baseUrl()
@@ -225,7 +218,93 @@ public class WebhooksAcceptanceTest {
                     .withHeader("X-Single", "{{math 1 '+' 2}}")
                     .withHeader("X-Multi", "{{math 3 'x' 2}}", "{{parameters.one}}")
                     .withBody("{{jsonPath originalRequest.body '$.name'}}")
-                    .withExtraParameter("one", "param-one-value"))));
+                    .withExtraParameter("one", "param-one-value")));
+
+    verify(0, postRequestedFor(anyUrl()));
+
+    client.postJson(
+        "/templating",
+        "{\n"
+            + "  \"callbackPath\": \"/callback/123\",\n"
+            + "  \"method\": \"POST\",\n"
+            + "  \"name\": \"Tom\"\n"
+            + "}");
+
+    waitForRequestToTargetServer();
+
+    LoggedRequest request =
+        targetServer.findAll(postRequestedFor(urlEqualTo("/callback/123"))).get(0);
+
+    assertThat(request.header("X-Single").firstValue(), is("3"));
+    assertThat(request.header("X-Multi").values(), hasItems("6", "param-one-value"));
+    assertThat(request.getBodyAsString(), is("Tom"));
+  }
+
+  public void appliesTemplatingToUrlMethodHeadersAndBodyFileNameViaJSON() throws Exception {
+    client.postJson(
+        "/__admin/mappings",
+        "{\n"
+            + "  \"id\" : \"8a58e190-4a83-4244-a064-265fcca46884\",\n"
+            + "  \"request\" : {\n"
+            + "    \"urlPath\" : \"/templating\",\n"
+            + "    \"method\" : \"POST\"\n"
+            + "  },\n"
+            + "  \"response\" : {\n"
+            + "    \"status\" : 200\n"
+            + "  },\n"
+            + "  \"uuid\" : \"8a58e190-4a83-4244-a064-265fcca46884\",\n"
+            + "  \"postServeActions\" : [{\n"
+            + "    \"name\" : \"webhook\",\n"
+            + "    \"parameters\" : {\n"
+            + "      \"method\" : \"{{jsonPath originalRequest.body '$.method'}}\",\n"
+            + "      \"url\" : \""
+            + targetServer.baseUrl()
+            + "{{{jsonPath originalRequest.body '$.callbackPath'}}}\",\n"
+            + "      \"headers\" : {\n"
+            + "        \"X-Single\" : \"{{math 1 '+' 2}}\",\n"
+            + "        \"X-Multi\" : [ \"{{math 3 'x' 2}}\", \"{{parameters.one}}\" ]\n"
+            + "      },\n"
+            + "      \"bodyFileName\" : \"myFile.json\",\n"
+            + "      \"one\" : \"param-one-value\"\n"
+            + "    }\n"
+            + "  }]\n"
+            + "}\n");
+
+    verify(0, postRequestedFor(anyUrl()));
+
+    client.postJson(
+        "/templating",
+        "{\n"
+            + "  \"callbackPath\": \"/callback/123\",\n"
+            + "  \"method\": \"POST\",\n"
+            + "  \"name\": \"Tom\"\n"
+            + "}");
+
+    waitForRequestToTargetServer();
+
+    LoggedRequest request =
+        targetServer.findAll(postRequestedFor(urlEqualTo("/callback/123"))).get(0);
+
+    assertThat(request.header("X-Single").firstValue(), is("3"));
+    assertThat(request.header("X-Multi").values(), hasItems("6", "param-one-value"));
+    assertThat(request.getBodyAsString(), is("Tom"));
+  }
+
+  public void appliesTemplatingToUrlMethodHeadersAndBodyViaDSL() throws Exception {
+    rule.stubFor(
+        post(urlPathEqualTo("/templating"))
+            .willReturn(ok())
+            .withPostServeAction(
+                "webhook",
+                webhook()
+                    .withMethod("{{jsonPath originalRequest.body '$.method'}}")
+                    .withUrl(
+                        targetServer.baseUrl()
+                            + "{{{jsonPath originalRequest.body '$.callbackPath'}}}")
+                    .withHeader("X-Single", "{{math 1 '+' 2}}")
+                    .withHeader("X-Multi", "{{math 3 'x' 2}}", "{{parameters.one}}")
+                    .withBody("{{jsonPath originalRequest.body '$.name'}}")
+                    .withExtraParameter("one", "param-one-value")));
 
     verify(0, postRequestedFor(anyUrl()));
 
@@ -273,11 +352,7 @@ public class WebhooksAcceptanceTest {
             + "        \"X-Single\" : \"{{math 1 '+' 2}}\",\n"
             + "        \"X-Multi\" : [ \"{{math 3 'x' 2}}\", \"{{parameters.one}}\" ]\n"
             + "      },\n"
-            + (bodyFileNameParam.isEmpty()?
-            "      \"body\" : \"{{jsonPath originalRequest.body '$.name'}}\",\n"
-                    :
-            "      \"bodyFileName\" : \"" + bodyFileNameParam + "\",\n"
-                    )
+            + "      \"body\" : \"{{jsonPath originalRequest.body '$.name'}}\",\n"
             + "      \"one\" : \"param-one-value\"\n"
             + "    }\n"
             + "  }]\n"
