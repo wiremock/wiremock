8d35dab72786fa8592f3b1ce14fc668b0e4aa17e
Greg Smith <gregoryryansmith@gmail.com>
2023-07-18 08:31:56 -0700

    Addresses additional failing Spotless checks

--- src/main/java/com/github/tomakehurst/wiremock/http/trafficlistener/CollectingNetworkTrafficListener.java
+++ src/main/java/com/github/tomakehurst/wiremock/http/trafficlistener/CollectingNetworkTrafficListener.java
@@ -23,7 +23,6 @@ import java.nio.ByteBuffer;
 import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
-import java.nio.charset.StandardCharsets;
 
 public class CollectingNetworkTrafficListener implements WiremockNetworkTrafficListener {
 
