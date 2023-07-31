9fc691f4a68fdb25bcafb5252833295b7c28dda6
Kirill Peshin <kirill.peshin@bscmsc.ru>
2023-07-31 07:23:38 +0300

    update

--- src/main/java/com/github/tomakehurst/wiremock/http/Response.java
+++ src/main/java/com/github/tomakehurst/wiremock/http/Response.java
@@ -21,7 +21,6 @@ import static java.net.HttpURLConnection.HTTP_NOT_FOUND;
 import static java.net.HttpURLConnection.HTTP_OK;
 
 import com.github.tomakehurst.wiremock.common.*;
-import com.google.common.io.ByteStreams;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Optional;
@@ -103,12 +102,9 @@ public class Response {
         return null;
       }
 
-      InputStream trimmedStream =
-          limit != null && !limit.isUnlimited()
-              ? ByteStreams.limit(stream, limit.getValue())
-              : stream;
-
-      return ByteStreams.toByteArray(trimmedStream);
+      return limit != null && !limit.isUnlimited()
+          ? stream.readNBytes(limit.getValue())
+          : stream.readAllBytes();
     }
   }
 
@@ -249,9 +245,7 @@ public class Response {
     private void addDelayIfSpecifiedGloballyOrIn(Integer fixedDelay, Integer globalFixedDelay) {
       Optional<Integer> optionalDelay =
           getDelayFromResponseOrGlobalSetting(fixedDelay, globalFixedDelay);
-      if (optionalDelay.isPresent()) {
-        incrementInitialDelay(optionalDelay.get());
-      }
+      optionalDelay.ifPresent(this::incrementInitialDelay);
     }
 
     private Optional<Integer> getDelayFromResponseOrGlobalSetting(
