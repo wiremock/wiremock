7f5a8b30bda09f116a0dea74836a2ec78bdc1b8d
Greg Smith <gregoryryansmith@gmail.com>
2023-08-02 18:17:08 -0700

    Use streams where appropriate and normalize test names

--- src/main/java/com/github/tomakehurst/wiremock/recording/SnapshotStubMappingPostProcessor.java
+++ src/main/java/com/github/tomakehurst/wiremock/recording/SnapshotStubMappingPostProcessor.java
@@ -15,11 +15,14 @@
  */
 package com.github.tomakehurst.wiremock.recording;
 
+import static java.util.stream.Collectors.toList;
+
 import com.github.tomakehurst.wiremock.matching.RequestPattern;
 import com.github.tomakehurst.wiremock.stubbing.StubMapping;
 import com.google.common.collect.HashMultiset;
 import com.google.common.collect.Multiset;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -48,12 +51,10 @@ public class SnapshotStubMappingPostProcessor {
     this.bodyExtractor = bodyExtractor;
   }
 
-  public List<StubMapping> process(Iterable<StubMapping> stubMappings) {
+  public List<StubMapping> process(Collection<StubMapping> stubMappings) {
     // 1. Run any applicable StubMappingTransformers against the stub mappings.
-    ArrayList<StubMapping> transformedStubMappings = new ArrayList<>();
-    for (StubMapping stubMapping : stubMappings) {
-      transformedStubMappings.add(transformerRunner.apply(stubMapping));
-    }
+    List<StubMapping> transformedStubMappings =
+        stubMappings.stream().map(transformerRunner).collect(toList());
 
     // 2. Detect duplicate requests and either discard them or turn them into scenarios.
     Multiset<RequestPattern> requestCounts = HashMultiset.create();
