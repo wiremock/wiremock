234cb0adfb53f03cce608f6c61e1d81a1a202706
Ifeanyi Kalu <kin@Abiriba.attlocal.net>
2023-08-07 22:00:02 -0500

    Add simple healthcheck api

--- src/main/java/com/github/tomakehurst/wiremock/core/Admin.java
+++ src/main/java/com/github/tomakehurst/wiremock/core/Admin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2022 Thomas Akehurst
+ * Copyright (C) 2011-2023 Thomas Akehurst
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,7 +28,7 @@ import com.github.tomakehurst.wiremock.stubbing.StubMapping;
 import com.github.tomakehurst.wiremock.verification.*;
 import java.util.UUID;
 
-public interface Admin {
+public interface Admin extends HealthChecker {
 
   void addStubMapping(StubMapping stubMapping);
 
