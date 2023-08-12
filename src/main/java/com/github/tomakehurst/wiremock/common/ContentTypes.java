436372f70add5aebb769b45fe3e37f19399295a7
Kirill Peshin <kirill.peshin@bscmsc.ru>
2023-08-12 14:44:58 +0300

    update

--- src/main/java/com/github/tomakehurst/wiremock/common/ContentTypes.java
+++ src/main/java/com/github/tomakehurst/wiremock/common/ContentTypes.java
@@ -34,6 +34,7 @@ public class ContentTypes {
   public static final String CONTENT_TYPE = "Content-Type";
   public static final String CONTENT_LENGTH = "Content-Length";
   public static final String CONTENT_ENCODING = "Content-Encoding";
+  public static final String TRANSFER_ENCODING = "Transfer-Encoding";
   public static final String LOCATION = "Location";
   public static final String AUTHORIZATION = "Authorization";
   public static final String COOKIE = "Cookie";
