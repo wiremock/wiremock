77f9b4d3a4b3052797e01e10e43be975603bb1c4
Oleg Nenashev <o.v.nenashev@gmail.com>
2023-08-29 12:37:06 +0200

    Add annotations for Beta and Internal APIs (#2332)
    * Add annotations for Beta and Internal APIs
    
    * Spotless
    
    * Excluded transitive jsr305 dependency from build to avoid javax.annotation classes ending up in the standalone JAR
    
    * Replace Guava by JDK (Partly)
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Replace Guava by JDK (Partly)
    
    * Replace Guava by JDK (Partly)
    
    * Replace Guava by JDK (Partly)
    
    * update
    
    * update
    
    * Shadow additional packages (#2327)
    
    * Removed redundant relocation of javax.annotation from the build
    
    * Fixed Spotless errors
    
    ---------
    
    Co-authored-by: Tom Akehurst <tom@wiremock.org>
    Co-authored-by: Kirill Peshin <kirill.peshin@bscmsc.ru>
    Co-authored-by: jluehe <janluehe@yahoo.com>

--- src/main/java/com/github/tomakehurst/wiremock/store/files/BlobStoreTextFile.java
+++ src/main/java/com/github/tomakehurst/wiremock/store/files/BlobStoreTextFile.java
@@ -21,7 +21,9 @@ import com.github.tomakehurst.wiremock.admin.NotFoundException;
 import com.github.tomakehurst.wiremock.common.TextFile;
 import com.github.tomakehurst.wiremock.store.BlobStore;
 import java.io.InputStream;
+import org.wiremock.annotations.Beta;
 
+@Beta(justification = "Externalized State API: https://github.com/wiremock/wiremock/issues/2144")
 public class BlobStoreTextFile extends TextFile {
 
   private final BlobStore blobStore;
