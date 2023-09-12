9b6c86764d2b137772a4584d901f30ebd4d07baa
Kirill Peshin <kirill.peshin@bscmsc.ru>
2023-09-12 07:27:00 +0300

    update

--- src/test/java/com/github/tomakehurst/wiremock/verification/diff/PlainTextDiffRendererTest.java
+++ src/test/java/com/github/tomakehurst/wiremock/verification/diff/PlainTextDiffRendererTest.java
@@ -57,7 +57,7 @@ class PlainTextDiffRendererTest {
   }
 
   @Test
-  public void rendersWithDifferingUrlHeaderAndJsonBody() {
+  void rendersWithDifferingUrlHeaderAndJsonBody() {
     Diff diff =
         new Diff(
             post("/thing")
