726ebbbad369a2d460e08d2e7677c68e59c8d653
Tom Akehurst <tom@wiremock.org>
2023-09-13 22:08:12 +0100

    Added ServeEventListener interface to webhooks so that both legacy PostServeAction and this can be used

--- src/test/java/com/github/tomakehurst/wiremock/WebhooksAcceptanceViaPostServeActionTest.java
+++ src/test/java/com/github/tomakehurst/wiremock/WebhooksAcceptanceViaPostServeActionTest.java
@@ -50,7 +50,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
-public class WebhooksAcceptanceTest {
+public class WebhooksAcceptanceViaPostServeActionTest {
 
   CountDownLatch latch;
 
