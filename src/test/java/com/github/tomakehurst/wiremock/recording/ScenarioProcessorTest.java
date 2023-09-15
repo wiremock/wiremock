54aeccc0f784a60c62b33b0f1b00018232b0ba11
Tom Akehurst <tom@wiremock.org>
2023-09-15 10:30:13 +0100

    Fixed #2364 - scenarios produced by recorder play back in reverse order (#2377)

--- src/test/java/com/github/tomakehurst/wiremock/recording/ScenarioProcessorTest.java
+++ src/test/java/com/github/tomakehurst/wiremock/recording/ScenarioProcessorTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2021 Thomas Akehurst
+ * Copyright (C) 2017-2023 Thomas Akehurst
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
  */
 package com.github.tomakehurst.wiremock.recording;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.ok;
 import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -23,6 +24,8 @@ import static org.hamcrest.Matchers.nullValue;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.stubbing.Scenario;
 import com.github.tomakehurst.wiremock.stubbing.StubMapping;
+import java.util.Collections;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class ScenarioProcessorTest {
@@ -31,32 +34,34 @@ public class ScenarioProcessorTest {
 
   @Test
   public void placesStubMappingsIntoScenariosWhenRepetitionsArePresent() {
-    StubMapping foobar1 = WireMock.get("/foo/bar").build();
-    StubMapping other1 = WireMock.get("/other").build();
-    StubMapping foobar2 = WireMock.get("/foo/bar").build();
-    StubMapping foobar3 = WireMock.get("/foo/bar").build();
-    StubMapping other2 = WireMock.get("/other").build();
+    StubMapping foobar1 = WireMock.get("/foo/bar").willReturn(ok("fb-1")).build();
+    StubMapping other1 = WireMock.get("/other").willReturn(ok("o-1")).build();
+    StubMapping foobar2 = WireMock.get("/foo/bar").willReturn(ok("fb-2")).build();
+    StubMapping foobar3 = WireMock.get("/foo/bar").willReturn(ok("fb-3")).build();
+    StubMapping other2 = WireMock.get("/other").willReturn(ok("o-2")).build();
 
-    processor.putRepeatedRequestsInScenarios(asList(foobar1, other1, foobar2, foobar3, other2));
+    List<StubMapping> stubs = asList(foobar1, other1, foobar2, foobar3, other2);
+    Collections.reverse(stubs);
+    processor.putRepeatedRequestsInScenarios(stubs);
 
-    assertThat(foobar1.getScenarioName(), is("scenario-1-foo-bar"));
+    assertThat(foobar1.getScenarioName(), is("scenario-2-foo-bar"));
     assertThat(foobar1.getRequiredScenarioState(), is(Scenario.STARTED));
-    assertThat("scenario-1-foo-bar-2", is(foobar1.getNewScenarioState()));
+    assertThat(foobar1.getNewScenarioState(), is("scenario-2-foo-bar-2"));
 
     assertThat(foobar2.getScenarioName(), is(foobar1.getScenarioName()));
-    assertThat(foobar2.getRequiredScenarioState(), is("scenario-1-foo-bar-2"));
-    assertThat(foobar2.getNewScenarioState(), is("scenario-1-foo-bar-3"));
+    assertThat(foobar2.getRequiredScenarioState(), is("scenario-2-foo-bar-2"));
+    assertThat(foobar2.getNewScenarioState(), is("scenario-2-foo-bar-3"));
 
     assertThat(foobar1.getScenarioName(), is(foobar3.getScenarioName()));
-    assertThat(foobar3.getRequiredScenarioState(), is("scenario-1-foo-bar-3"));
+    assertThat(foobar3.getRequiredScenarioState(), is("scenario-2-foo-bar-3"));
     assertThat(
         "Last mapping should not have a state transition",
         foobar3.getNewScenarioState(),
         nullValue());
 
-    assertThat(other1.getScenarioName(), is("scenario-2-other"));
-    assertThat(other1.getNewScenarioState(), is("scenario-2-other-2"));
-    assertThat(other2.getRequiredScenarioState(), is("scenario-2-other-2"));
+    assertThat(other1.getScenarioName(), is("scenario-1-other"));
+    assertThat(other1.getNewScenarioState(), is("scenario-1-other-2"));
+    assertThat(other2.getRequiredScenarioState(), is("scenario-1-other-2"));
   }
 
   @Test
