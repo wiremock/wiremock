7672aa5ff79a12494bb9b0fdefb358d1cd052255
Kirill Peshin <kirill.peshin@bscmsc.ru>
2023-09-19 07:39:09 +0300

    Replace Guava by JDK (Partly)

--- src/test/java/com/github/tomakehurst/wiremock/matching/StringValuePatternTest.java
+++ src/test/java/com/github/tomakehurst/wiremock/matching/StringValuePatternTest.java
@@ -18,8 +18,6 @@ package com.github.tomakehurst.wiremock.matching;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.reflect.ClassPath;
-
 import java.io.*;
 import java.lang.reflect.Modifier;
 import java.net.URL;
@@ -27,128 +25,67 @@ import java.util.*;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
-@SuppressWarnings("rawtypes")
+@SuppressWarnings({"rawtypes", "unchecked"})
 class StringValuePatternTest {
 
   @Test
   void allSubclassesHaveWorkingToString() throws Exception {
-    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+    Set<Class> matchingClasses = getClassOfPackage("com.github.tomakehurst.wiremock.matching");
 
-    Set<ClassPath.ClassInfo> allClasses = ClassPath.from(classLoader).getAllClasses();
-
-    Set<Class> newAllClasses =
-            getClassOfPackage("com.github.tomakehurst.wiremock.matching");
-
-    var newAllClassesAfterFilter01 =
-        newAllClasses.stream()
-            .filter(
-                classInfo ->
-                    classInfo
-                        .getPackageName()
-                        .startsWith("com.github.tomakehurst.wiremock.matching"))
-                .sorted(Comparator.comparing(Class::getName))
-            .collect(Collectors.toCollection(LinkedHashSet::new));
-
-    var allClassesAfterFilter01 =
-        allClasses.stream()
-            .filter(
-                classInfo ->
-                    classInfo
-                        .getPackageName()
-                        .startsWith("com.github.tomakehurst.wiremock.matching"))
-                .sorted(Comparator.comparing(ClassPath.ClassInfo::getName))
-                .collect(Collectors.toCollection(LinkedHashSet::new));
-
-    var allClassesAfterMap =
-        allClassesAfterFilter01.stream()
-            .map(
-                input -> {
-                  try {
-                    return input.load();
-                  } catch (Throwable e) {
-                    return Object.class;
-                  }
-                })
-            .collect(Collectors.toSet());
-
-    var allClassesAfterFilter02 =
-        allClassesAfterMap.stream()
+    var matchingClassesAfterFilter01 =
+        matchingClasses.stream()
             .filter(clazz -> clazz.isAssignableFrom(StringValuePattern.class))
             .collect(Collectors.toSet());
 
-    var allClassesAfterFilter03 =
-        allClassesAfterFilter02.stream()
+    var matchingClassesAfterFilter02 =
+        matchingClassesAfterFilter01.stream()
             .filter(clazz -> !Modifier.isAbstract(clazz.getModifiers()))
             .collect(Collectors.toSet());
 
     assertDoesNotThrow(
-        () -> allClassesAfterFilter03.forEach(this::findConstructorWithStringParamInFirstPosition));
+        () ->
+            matchingClassesAfterFilter02.forEach(
+                this::findConstructorWithStringParamInFirstPosition));
   }
 
-    public Set<Class> getClassOfPackage(String packageName)
-            throws ClassNotFoundException, IOException {
+  public Set<Class> getClassOfPackage(String packageName)
+      throws ClassNotFoundException, IOException {
 
-        ClassLoader classLoader = Thread.currentThread()
-                .getContextClassLoader();
+    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 
-        assert classLoader != null;
-        String path = packageName.replace('.', '/');
-        Enumeration<URL> resources = classLoader.getResources(path);
-        List<File> dirs = new ArrayList<>();
-        while (resources.hasMoreElements()) {
-            URL resource = resources.nextElement();
-            dirs.add(new File(resource.getFile()));
-        }
-        Set<Class> classes = new HashSet<>();
-        for (File directory : dirs) {
-            classes.addAll(findClasses(directory, packageName));
-        }
-        return classes;
+    assert classLoader != null;
+    String path = packageName.replace('.', '/');
+    Enumeration<URL> resources = classLoader.getResources(path);
+    List<File> dirs = new ArrayList<>();
+    while (resources.hasMoreElements()) {
+      URL resource = resources.nextElement();
+      dirs.add(new File(resource.getFile()));
     }
-
-    private Set<Class> findClasses(File directory, String packageName)
-            throws ClassNotFoundException {
-        Set<Class> classes = new HashSet<>();
-        if (!directory.exists()) {
-            return classes;
-        }
-        File[] files = directory.listFiles();
-        for (File file : Objects.requireNonNull(files)) {
-            if (file.isDirectory()) {
-                assert !file.getName().contains(".");
-                classes.addAll(findClasses(file,
-                        packageName + "." + file.getName()));
-            } else if (file.getName().endsWith(".class")) {
-                classes.add(Class.forName(packageName
-                        + '.'
-                        + file.getName().substring(0,
-                        file.getName().length() - 6)));
-            }
-        }
-        return classes;
+    Set<Class> classes = new HashSet<>();
+    for (File directory : dirs) {
+      classes.addAll(findClasses(directory, packageName));
     }
+    return classes;
+  }
 
-//  public Set<Class> findAllClassesUsingClassLoader(String packageName) {
-//    BufferedReader reader;
-//    try (InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream(packageName.replaceAll("[.]", "/"))) {
-//      reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(stream)));
-//    } catch (IOException ignored) {
-//      return Collections.emptySet();
-//    }
-//    return reader
-//        .lines()
-//        .filter(line -> line.endsWith(".class"))
-//        .map(line -> getClass(line, packageName))
-//        .collect(Collectors.toSet());
-//  }
-
-//  private Class getClass(String className, String packageName) {
-//    try {
-//      return Class.forName(packageName + "." + className.substring(0, className.lastIndexOf('.')));
-//    } catch (ClassNotFoundException ignored) {
-//    }
-//    return null;
-//  }
+  private Set<Class> findClasses(File directory, String packageName) throws ClassNotFoundException {
+    Set<Class> classes = new HashSet<>();
+    if (!directory.exists()) {
+      return classes;
+    }
+    File[] files = directory.listFiles();
+    for (File file : Objects.requireNonNull(files)) {
+      if (file.isDirectory()) {
+        assert !file.getName().contains(".");
+        classes.addAll(findClasses(file, packageName + "." + file.getName()));
+      } else if (file.getName().endsWith(".class")) {
+        classes.add(
+            Class.forName(
+                packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+      }
+    }
+    return classes;
+  }
 
   private void findConstructorWithStringParamInFirstPosition(Class<?> clazz) {
     Arrays.stream(clazz.getConstructors())
