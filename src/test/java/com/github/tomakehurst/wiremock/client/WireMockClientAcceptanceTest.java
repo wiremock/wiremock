c14d29f6bff294139b350fefb1620cf318bab191
Robert Elliot <rob@lidalia.org.uk>
2023-09-29 10:46:59 +0100

    Allow empty URI path segments after the first
    [RFC 3986](https://datatracker.ietf.org/doc/html/rfc3986#section-3.3)
    disallows an empty segment for the _first_ segment of the path, but
    permits it in any remaining segments.
    
    Jetty 9+ is stricter by default, disallowing _all_ empty segments.
    
    We can revert to the older behaviour, allowing more general mocking, by
    setting Jetty's UriCompliance to UNSAFE.

--- src/test/java/com/github/tomakehurst/wiremock/client/WireMockClientAcceptanceTest.java
+++ src/test/java/com/github/tomakehurst/wiremock/client/WireMockClientAcceptanceTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2021 Thomas Akehurst
+ * Copyright (C) 2011-2023 Thomas Akehurst
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -69,5 +69,12 @@ public class WireMockClientAcceptanceTest {
 
     assertThat(
         testClient.get("/my/new/resource").content(), is("{\"address\":\"Puerto Banús, Málaga\"}"));
+  }
+
+  @Test
+  public void buildsMappingWithEmptySegment() {
+    givenThat(get(urlEqualTo("/my//resource")).willReturn(aResponse().withStatus(200)));
+
+    assertThat(testClient.get("/my//resource").statusCode(), is(200));
   }
 }
