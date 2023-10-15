fb583f9ecb73693af7b453603cd665cc123b1147
leeturner <lee@leeturner.me>
2023-10-15 17:43:12 +0100

    If the version cli option is set, output the current wiremock version and then exit.

--- src/main/java/com/github/tomakehurst/wiremock/standalone/WireMockServerRunner.java
+++ src/main/java/com/github/tomakehurst/wiremock/standalone/WireMockServerRunner.java
@@ -27,6 +27,7 @@ import static java.lang.System.out;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.common.FatalStartupException;
 import com.github.tomakehurst.wiremock.common.FileSource;
+import com.github.tomakehurst.wiremock.core.Version;
 import com.github.tomakehurst.wiremock.http.ResponseDefinition;
 import com.github.tomakehurst.wiremock.matching.RequestPattern;
 import com.github.tomakehurst.wiremock.stubbing.StubMapping;
@@ -57,6 +58,10 @@ public class WireMockServerRunner {
     CommandLineOptions options = new CommandLineOptions(args);
     if (options.help()) {
       out.println(options.helpText());
+      return;
+    }
+    if (options.version()) {
+      out.println(Version.getCurrentVersion());
       return;
     }
 
