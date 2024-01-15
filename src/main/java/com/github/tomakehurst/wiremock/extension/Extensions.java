c5442b8356e18f885ca2c6266d9e57c1a62bc99f
Kyle Winkelman <kyle.winkelman@optum.com>
2024-01-15 10:35:28 -0600

    Supply lazy TemplateEngine, HttpClientFactory, and DefaultHttpClient.

--- src/main/java/com/github/tomakehurst/wiremock/extension/Extensions.java
+++ src/main/java/com/github/tomakehurst/wiremock/extension/Extensions.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Thomas Akehurst
+ * Copyright (C) 2023-2024 Thomas Akehurst
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,10 +23,13 @@ import com.github.jknack.handlebars.Helper;
 import com.github.tomakehurst.wiremock.common.FileSource;
 import com.github.tomakehurst.wiremock.core.Admin;
 import com.github.tomakehurst.wiremock.core.Options;
+import com.github.tomakehurst.wiremock.extension.responsetemplating.LazyTemplateEngine;
 import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
 import com.github.tomakehurst.wiremock.extension.responsetemplating.TemplateEngine;
 import com.github.tomakehurst.wiremock.http.client.HttpClient;
 import com.github.tomakehurst.wiremock.http.client.HttpClientFactory;
+import com.github.tomakehurst.wiremock.http.client.LazyHttpClient;
+import com.github.tomakehurst.wiremock.http.client.LazyHttpClientFactory;
 import com.github.tomakehurst.wiremock.store.Stores;
 import java.util.*;
 import java.util.concurrent.Executors;
@@ -191,17 +194,18 @@ public class Extensions implements WireMockServices {
 
   @Override
   public TemplateEngine getTemplateEngine() {
-    return templateEngine;
+    return new LazyTemplateEngine(() -> templateEngine);
   }
 
   @Override
   public HttpClientFactory getHttpClientFactory() {
-    return httpClientFactory;
+    return new LazyHttpClientFactory(() -> httpClientFactory);
   }
 
   @Override
   public HttpClient getDefaultHttpClient() {
-    return httpClientFactory.buildHttpClient(options, true, Collections.emptyList(), true);
+    return new LazyHttpClient(
+        () -> httpClientFactory.buildHttpClient(options, true, Collections.emptyList(), true));
   }
 
   public int getCount() {
