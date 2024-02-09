392da86e020dbe35d586932ec3d72c7f6c1d329f
Andriy Redko <drreta@gmail.com>
2024-02-08 20:11:23 -0500

    Refactor JettyHttpServerFactory to use Jetty.VERSION to detect the version of the Jetty
    Signed-off-by: Andriy Redko <drreta@gmail.com>

--- src/main/java/com/github/tomakehurst/wiremock/jetty/JettyHttpServerFactory.java
+++ src/main/java/com/github/tomakehurst/wiremock/jetty/JettyHttpServerFactory.java
@@ -26,26 +26,11 @@ import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.lang.invoke.WrongMethodTypeException;
+import org.eclipse.jetty.util.Jetty;
 
 public class JettyHttpServerFactory implements HttpServerFactory {
-  private static final MethodHandle SERVER_CONSTRUCTOR = getServerConstructor();
-
-  @SuppressWarnings("unchecked")
-  private static MethodHandle getServerConstructor() {
-    try {
-      Class<? extends JettyHttpServer> serverClass =
-          (Class<? extends JettyHttpServer>)
-              Class.forName("com.github.tomakehurst.wiremock.jetty12.Jetty12HttpServer");
-      return safelyGetConstructor(
-          serverClass, Options.class, AdminRequestHandler.class, StubRequestHandler.class);
-    } catch (ClassNotFoundException e) {
-      return safelyGetConstructor(
-          Jetty11HttpServer.class,
-          Options.class,
-          AdminRequestHandler.class,
-          StubRequestHandler.class);
-    }
-  }
+  private static final String JETTY_11 = "11"; /* Jetty 11 */
+  private static final String JETTY_12 = "12"; /* Jetty 12 */
 
   private static MethodHandle safelyGetConstructor(Class<?> clazz, Class<?>... parameterTypes) {
     try {
@@ -62,8 +47,27 @@ public class JettyHttpServerFactory implements HttpServerFactory {
       AdminRequestHandler adminRequestHandler,
       StubRequestHandler stubRequestHandler) {
     try {
-      return (HttpServer)
-          SERVER_CONSTRUCTOR.invoke(options, adminRequestHandler, stubRequestHandler);
+      final String[] version = Jetty.VERSION.split("[.]");
+      if (version.length == 0 || JETTY_11.equalsIgnoreCase(version[0])) {
+        return new Jetty11HttpServer(options, adminRequestHandler, stubRequestHandler);
+      } else if (JETTY_12.equalsIgnoreCase(version[0])) {
+        // Using the reflection at the moment since Jetty12HttpServer is not visible
+        // during the compilation of main source set.
+        @SuppressWarnings("unchecked")
+        final Class<? extends JettyHttpServer> serverClass =
+            (Class<? extends JettyHttpServer>)
+                Class.forName("com.github.tomakehurst.wiremock.jetty12.Jetty12HttpServer");
+
+        return (HttpServer)
+            safelyGetConstructor(
+                    serverClass, Options.class, AdminRequestHandler.class, StubRequestHandler.class)
+                .invoke(options, adminRequestHandler, stubRequestHandler);
+      } else {
+        throw new IllegalStateException(
+            "Unsupported Jetty version "
+                + version[0]
+                + ", only Jetty 11/12 are supported at the moment");
+      }
     } catch (WrongMethodTypeException | ClassCastException e) {
       return Exceptions.throwUnchecked(e, HttpServer.class);
     } catch (Throwable e) {
--- src/test/java/com/github/tomakehurst/wiremock/jetty11/archunit/UnusedCodeTest.java
+++ src/test/java/com/github/tomakehurst/wiremock/jetty11/archunit/UnusedCodeTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021-2023 Thomas Akehurst
+ * Copyright (C) 2021-2024 Thomas Akehurst
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.tomakehurst.wiremock.archunit;
+package com.github.tomakehurst.wiremock.jetty11.archunit;
 
 import static com.tngtech.archunit.base.DescribedPredicate.describe;
 import static com.tngtech.archunit.base.DescribedPredicate.not;
