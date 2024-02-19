2d5e6637274965b3bc7bc0bc15761bf75c1f5978
Tom Akehurst <tom@wiremock.org>
2024-02-19 19:05:00 +0000

    Fixed formatting issues

--- src/main/java/com/github/tomakehurst/wiremock/stubbing/SubEvent.java
+++ src/main/java/com/github/tomakehurst/wiremock/stubbing/SubEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Thomas Akehurst
+ * Copyright (C) 2023-2024 Thomas Akehurst
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,6 @@ package com.github.tomakehurst.wiremock.stubbing;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.tomakehurst.wiremock.common.Json;
 import com.github.tomakehurst.wiremock.common.Message;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -32,9 +31,8 @@ public class SubEvent {
   public static final String WARNING = "WARNING";
   public static final String ERROR = "ERROR";
 
-  private static final List<String> STANDARD_TYPES = Arrays.asList(
-      NON_MATCH_TYPE, JSON_ERROR, XML_ERROR, INFO, WARNING, ERROR
-  );
+  private static final List<String> STANDARD_TYPES =
+      Arrays.asList(NON_MATCH_TYPE, JSON_ERROR, XML_ERROR, INFO, WARNING, ERROR);
 
   private final String type;
 
@@ -92,9 +90,10 @@ public class SubEvent {
   }
 
   public boolean isEquivalentStandardTypedEventTo(SubEvent other) {
-    return isStandardType() && other.isStandardType() &&
-            type.equals(other.type) &&
-            data.equals(other.data);
+    return isStandardType()
+        && other.isStandardType()
+        && type.equals(other.type)
+        && data.equals(other.data);
   }
 
   boolean isStandardType() {
