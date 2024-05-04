de27363a06f5ef87fee916a101dff7543e7da57c
rafearnold <rafe.arnold@gmail.com>
2024-05-04 10:00:07 +0100

    fix: handlebars math helper handles large negative integers

--- src/main/java/com/github/tomakehurst/wiremock/extension/responsetemplating/helpers/MathsHelper.java
+++ src/main/java/com/github/tomakehurst/wiremock/extension/responsetemplating/helpers/MathsHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021-2022 Thomas Akehurst
+ * Copyright (C) 2021-2024 Thomas Akehurst
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -88,11 +88,12 @@ public class MathsHelper extends HandlebarsHelper<Object> {
     }
 
     if (value.scale() == 0) {
-      if (value.longValue() <= Integer.MAX_VALUE) {
+      long longValue = value.longValue();
+      if (longValue <= Integer.MAX_VALUE && longValue >= Integer.MIN_VALUE) {
         return value.intValue();
       }
 
-      return value.longValue();
+      return longValue;
     }
 
     return value.doubleValue();
