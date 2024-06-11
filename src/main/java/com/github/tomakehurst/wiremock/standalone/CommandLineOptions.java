3a918b4b352663eca8bd9fb1040c3135086a3625
leeturner <lee@leeturner.me>
2024-06-11 14:16:05 +0100

    Default the max template cache entries to 1000 instead of infinite

--- src/main/java/com/github/tomakehurst/wiremock/standalone/CommandLineOptions.java
+++ src/main/java/com/github/tomakehurst/wiremock/standalone/CommandLineOptions.java
@@ -988,8 +988,8 @@ public class CommandLineOptions implements Options {
   @Override
   public Long getMaxTemplateCacheEntries() {
     return optionSet.has(MAX_TEMPLATE_CACHE_ENTRIES)
-        ? Long.valueOf(optionSet.valueOf(MAX_TEMPLATE_CACHE_ENTRIES).toString())
-        : null;
+        ? Long.parseLong(optionSet.valueOf(MAX_TEMPLATE_CACHE_ENTRIES).toString())
+        : 1000;
   }
 
   @SuppressWarnings("unchecked")
