29f4432ce2f7a954afeba62b825d3c74ec852329
Ian Johnson <ian@ianjohnson.dev>
2024-12-17 00:10:32 -0500

    Always use the system default `TransformerFactory` in `FormatXmlHelper`
    Closes #2911
    Closes #2913
    
    The implementation of `FormatXmlHelper` relies on a non-standard `TransformerFactory` attribute, `indent-number`, which is not supported by all implementations. By using `TransformerFactory.newDefaultInstance` instead of `TransformerFactory.newInstance`, the system default implementation (`com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl` on OpenJDK-derived JDKs) will be used regardless of any other implementations which might be present on the classpath.

--- src/main/java/com/github/tomakehurst/wiremock/extension/responsetemplating/helpers/FormatXmlHelper.java
+++ src/main/java/com/github/tomakehurst/wiremock/extension/responsetemplating/helpers/FormatXmlHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 Thomas Akehurst
+ * Copyright (C) 2024-2025 Thomas Akehurst
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -64,7 +64,7 @@ public class FormatXmlHelper extends AbstractFormattingHelper {
   private final TransformerFactory transformerFactory;
 
   public FormatXmlHelper() {
-    TransformerFactory factory = TransformerFactory.newInstance();
+    TransformerFactory factory = TransformerFactory.newDefaultInstance();
     factory.setAttribute("indent-number", 2);
     factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
     factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, "");
