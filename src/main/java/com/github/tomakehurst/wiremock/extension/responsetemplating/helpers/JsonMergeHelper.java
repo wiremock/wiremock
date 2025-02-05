ea7272233d55388f40507624c15773a3b44e9827
Tom Akehurst <tom@wiremock.org>
2025-02-05 17:41:57 +0000

    Support optional removal of null-valued attributes in jsonMerge helper (#2957)
    * Added a removeNulls parameter to the jsonMerge helper which, when true indicates that attributes with null values in the right-hand JSON document should be removed completely from the result rather than setting to null
    
    * Added block test for jsonMerge removeNulls=true

--- src/main/java/com/github/tomakehurst/wiremock/extension/responsetemplating/helpers/JsonMergeHelper.java
+++ src/main/java/com/github/tomakehurst/wiremock/extension/responsetemplating/helpers/JsonMergeHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 Thomas Akehurst
+ * Copyright (C) 2024-2025 Thomas Akehurst
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -60,11 +60,14 @@ class JsonMergeHelper extends HandlebarsHelper<Object> {
       return handleError("JSON to merge is not a JSON object ('" + jsonToMergeString + "')");
     }
 
-    merge((ObjectNode) baseJson, (ObjectNode) jsonToMerge);
+    boolean removeNulls =
+        options.hash.containsKey("removeNulls") && (boolean) options.hash.get("removeNulls");
+
+    merge((ObjectNode) baseJson, (ObjectNode) jsonToMerge, removeNulls);
     return Json.getObjectMapper().writeValueAsString(baseJson);
   }
 
-  private void merge(ObjectNode base, ObjectNode other) {
+  private void merge(ObjectNode base, ObjectNode other, boolean removeNulls) {
     for (Iterator<Map.Entry<String, JsonNode>> it = other.fields(); it.hasNext(); ) {
       Map.Entry<String, JsonNode> child = it.next();
       String fieldName = child.getKey();
@@ -72,12 +75,16 @@ class JsonMergeHelper extends HandlebarsHelper<Object> {
       if (childNodeToMerge instanceof ObjectNode) {
         JsonNode baseChildNode = base.get(fieldName);
         if (baseChildNode instanceof ObjectNode) {
-          merge((ObjectNode) baseChildNode, (ObjectNode) childNodeToMerge);
+          merge((ObjectNode) baseChildNode, (ObjectNode) childNodeToMerge, removeNulls);
         } else {
           base.replace(fieldName, childNodeToMerge);
         }
       } else {
-        base.replace(fieldName, childNodeToMerge);
+        if (removeNulls && childNodeToMerge.isNull()) {
+          base.remove(fieldName);
+        } else {
+          base.replace(fieldName, childNodeToMerge);
+        }
       }
     }
   }
