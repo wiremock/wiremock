a63a93fc78199f6a1add9adb98ac996ae6cc2c61
Tom Akehurst <tom@wiremock.org>
2025-11-27 08:52:02 +0000

    Converted CustomMatcherDefinition to a record

--- wiremock-core/src/main/java/com/github/tomakehurst/wiremock/matching/CustomMatcherDefinition.java
+++ wiremock-core/src/main/java/com/github/tomakehurst/wiremock/matching/CustomMatcherDefinition.java
@@ -18,22 +18,5 @@ package com.github.tomakehurst.wiremock.matching;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.tomakehurst.wiremock.extension.Parameters;
 
-public class CustomMatcherDefinition {
-
-  private final String name;
-  private final Parameters parameters;
-
-  public CustomMatcherDefinition(
-      @JsonProperty("name") String name, @JsonProperty("parameters") Parameters parameters) {
-    this.name = name;
-    this.parameters = parameters;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public Parameters getParameters() {
-    return parameters;
-  }
-}
+public record CustomMatcherDefinition(
+    @JsonProperty("name") String name, @JsonProperty("parameters") Parameters parameters) {}
