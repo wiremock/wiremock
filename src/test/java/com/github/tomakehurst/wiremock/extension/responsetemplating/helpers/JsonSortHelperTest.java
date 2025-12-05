838c3b48996bdf2691296f856625d948f5510749
leeturner <lee@leeturner.me>
2025-12-05 09:37:43 +0000

    feat: Basic implementation of JsonSortHelper

---
+++ src/test/java/com/github/tomakehurst/wiremock/extension/responsetemplating/helpers/JsonSortHelperTest.java
@@ -0,0 +1,441 @@
+/*
+ * Copyright (C) 2025 Thomas Akehurst
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.tomakehurst.wiremock.extension.responsetemplating.helpers;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+
+import com.github.jknack.handlebars.EscapingStrategy;
+import com.github.jknack.handlebars.Handlebars;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junitpioneer.jupiter.json.JsonSource;
+
+public class JsonSortHelperTest extends HandlebarsHelperTestBase {
+
+  @ParameterizedTest
+  @JsonSource({
+    // have to double wrap arrays because @JsonSource unwraps them.
+    "[[ { id: 456, name: 'bob' }, { id: 123, name: 'alice' }, { id: 321, name: 'sam' } ]]",
+    "{ id: 456, name: 'bob' }",
+    "true",
+    "null",
+    "123",
+  })
+  void errorsIfInputJsonIsNotAString(Object inputJson) throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    Map<String, Object> context = new HashMap<>();
+    context.put("input", inputJson);
+    String output = handleBars.compileInline("{{ jsonSort input '$.name' }}").apply(context);
+    assertThat(output, is("[ERROR: Input JSON must be a string]"));
+  }
+
+  @Test
+  void errorsIfJsonpathIsNotProvided() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    Map<String, String> context = new HashMap<>();
+    context.put("input", """
+              {"id":123,"name":"bob"}""");
+    String output = handleBars.compileInline("{{ jsonSort input }}").apply(context);
+    assertThat(output, is("[ERROR: A single JSONPath expression parameter must be supplied]"));
+  }
+
+  @Test
+  void errorsIfMoreThanOneParameterIsProvided() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    Map<String, String> context = new HashMap<>();
+    context.put("input", """
+              {"id":123,"name":"bob"}""");
+    String output =
+        handleBars.compileInline("{{ jsonSort input '$.name' '$.name' }}").apply(context);
+    assertThat(output, is("[ERROR: A single JSONPath expression parameter must be supplied]"));
+  }
+
+  @ParameterizedTest
+  @JsonSource({
+    "{}",
+    // have to double wrap arrays because @JsonSource unwraps them.
+    "[[]]",
+    "true",
+    "null",
+    "123",
+  })
+  void errorsIfJsonpathExpressionIsNotAString(Object jsonPath) throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    Map<String, Object> context = new HashMap<>();
+    context.put("input", """
+            {"id":123,"name":"bob"}""");
+    context.put("jsonPath", jsonPath);
+    String output = handleBars.compileInline("{{ jsonSort input jsonPath }}").apply(context);
+    assertThat(output, is("[ERROR: JSONPath parameter must be a string]"));
+  }
+
+  @ParameterizedTest
+  @ValueSource(
+      strings = {
+        "invalid json",
+        "[ { \"id\": 456 } ",
+        "",
+        " ",
+      })
+  void errorsIfInputJsonStringIsNotValidJson(String inputJson) throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    Map<String, String> context = new HashMap<>();
+    context.put("input", inputJson);
+    String output = handleBars.compileInline("{{ jsonSort input '$[*].name' }}").apply(context);
+    assertThat(output, is("[ERROR: Input JSON string is not valid JSON ('" + inputJson + "')]"));
+  }
+
+  @Test
+  void errorsIfOrderParameterIsNotAString() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    Map<String, String> context = new HashMap<>();
+    context.put("input", """
+              [{"id":123,"name":"bob"}]""");
+    String output =
+        handleBars.compileInline("{{ jsonSort input '$[*].name' order=123 }}").apply(context);
+    assertThat(output, is("[ERROR: order parameter must be a string]"));
+  }
+
+  @ParameterizedTest
+  @ValueSource(
+      strings = {"invalid", "ascending", "descending", "up", "down", "ASC", "DESC", " ", ""})
+  void errorsIfOrderParameterIsNotAscOrDesc(String orderValue) throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    Map<String, String> context = new HashMap<>();
+    context.put("input", """
+              [{"id":123,"name":"bob"}]""");
+    String output =
+        handleBars
+            .compileInline("{{ jsonSort input '$[*].name' order='" + orderValue + "' }}")
+            .apply(context);
+    assertThat(output, is("[ERROR: order parameter must be 'asc' or 'desc']"));
+  }
+
+  @Test
+  void orderParameterIsOptionalAndDefaultsToAscending() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    Map<String, String> context = new HashMap<>();
+    context.put("input", """
+              [{"id":123,"name":"bob"}]""");
+    String output = handleBars.compileInline("{{ jsonSort input '$[*].name' }}").apply(context);
+    // Should not error - order parameter is optional
+    assertThat(output, not(containsString("ERROR")));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"$.name", "$.users.name", "$", "name"})
+  void errorsIfJsonPathDoesNotIncludeArrayWildcard(String jsonPath) throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    Map<String, String> context = new HashMap<>();
+    context.put("input", """
+              [{"id":123,"name":"bob"}]""");
+    String output =
+        handleBars.compileInline("{{ jsonSort input '" + jsonPath + "' }}").apply(context);
+    assertThat(
+        output,
+        is(
+            "[ERROR: JSONPath must include [*] to specify array location (e.g., '$[*].name' or '$.users[*].name')]"));
+  }
+
+  @Test
+  void acceptsValidJsonPathWithArrayWildcard() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    Map<String, String> context = new HashMap<>();
+    context.put("input", """
+              [{"id":123,"name":"bob"}]""");
+    String output = handleBars.compileInline("{{ jsonSort input '$[*].name' }}").apply(context);
+    // Should not error about missing [*]
+    assertThat(output, not(containsString("must include [*]")));
+  }
+
+  @Test
+  void errorsIfInputJsonIsNotAnArray() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    Map<String, String> context = new HashMap<>();
+    context.put("input", """
+              {"id":123,"name":"bob"}""");
+    String output = handleBars.compileInline("{{ jsonSort input '$[*].name' }}").apply(context);
+    assertThat(output, is("[ERROR: JSONPath does not reference an array ('$')]"));
+  }
+
+  @Test
+  void acceptsNestedArrayJsonPath() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    String input = """
+            {"users":[{"name":"bob"}]}""";
+    Map<String, String> context = new HashMap<>();
+    context.put("input", input);
+    String output =
+        handleBars.compileInline("{{ jsonSort input '$.users[*].name' }}").apply(context);
+    // Should not error - valid nested array path
+    assertThat(output, not(containsString("ERROR")));
+  }
+
+  @Test
+  void errorsIfJsonPathExpressionIsInvalid() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    Map<String, String> context = new HashMap<>();
+    context.put("input", """
+              [{"id":123,"name":"bob"}]""");
+    String output = handleBars.compileInline("{{ jsonSort input '$[*].[' }}").apply(context);
+    assertThat(output, is("[ERROR: Invalid JSONPath expression ('$[*].[')]"));
+  }
+
+  @Test
+  void errorsIfJsonPathDoesNotMatchAnyValues() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    // JsonPath $.users[*].name but input has no 'users' field
+    Map<String, String> context = new HashMap<>();
+    context.put("input", """
+              [{"id":123,"name":"bob"}]""");
+    String output =
+        handleBars.compileInline("{{ jsonSort input '$.users[*].name' }}").apply(context);
+    assertThat(
+        output, is("[ERROR: JSONPath expression did not match any values ('$.users[*].name')]"));
+  }
+
+  @Test
+  void errorsIfJsonPathDoesNotReferenceAnArray() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    // Input has 'user' as an object, not an array
+    String input = """
+            {"user":{"name":"bob"}}""";
+    Map<String, String> context = new HashMap<>();
+    context.put("input", input);
+    String output =
+        handleBars.compileInline("{{ jsonSort input '$.user[*].name' }}").apply(context);
+    assertThat(output, is("[ERROR: JSONPath does not reference an array ('$.user')]"));
+  }
+
+  @Test
+  void errorsIfRootIsNotAnArrayWhenUsingRootPath() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    // Root is an object, not an array
+    Map<String, String> context = new HashMap<>();
+    context.put("input", """
+              {"id":123,"name":"bob"}""");
+    String output = handleBars.compileInline("{{ jsonSort input '$[*].name' }}").apply(context);
+    assertThat(output, is("[ERROR: JSONPath does not reference an array ('$')]"));
+  }
+
+  @Test
+  void handlesComplexJsonPathGracefully() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    // Just verify we don't crash with complex JSON
+    String input = """
+            {"data":{"nested":{"users":[{"name":"bob"}]}}}""";
+    Map<String, String> context = new HashMap<>();
+    context.put("input", input);
+    String output =
+        handleBars
+            .compileInline("{{ jsonSort input '$.data.nested.users[*].name' }}")
+            .apply(context);
+    // Should work or give a clear error, not crash
+    assertThat(output, is(input));
+  }
+
+  @Test
+  void returnsEmptyArrayUnchanged() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    Map<String, String> context = new HashMap<>();
+    context.put("input", "[]");
+    String output = handleBars.compileInline("{{ jsonSort input '$[*].name' }}").apply(context);
+    assertThat(output, is("[]"));
+  }
+
+  @Test
+  void returnsSingleElementArrayUnchanged() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    String input = """
+            [{"id":123,"name":"alice"}]""";
+    Map<String, String> context = new HashMap<>();
+    context.put("input", input);
+    String output = handleBars.compileInline("{{ jsonSort input '$[*].name' }}").apply(context);
+    assertThat(output, is(input));
+  }
+
+  @Test
+  void errorsIfSortFieldIsMissingFromObjects() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    Map<String, String> context = new HashMap<>();
+    context.put("input", """
+              [{"id":123,"name":"bob"}]""");
+    String output =
+        handleBars.compileInline("{{ jsonSort input '$[*].missingField' }}").apply(context);
+    assertThat(
+        output,
+        is(
+            "[ERROR: All objects in the array must have the sort field specified by JSONPath expression ('$[*].missingField')]"));
+  }
+
+  @Test
+  void errorsIfSortFieldIsUnsupportedType() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    String input = """
+            [{"data":{"nested":"value"}},{"data":{"other":"value"}}]""";
+    Map<String, String> context = new HashMap<>();
+    context.put("input", input);
+    String output = handleBars.compileInline("{{ jsonSort input '$[*].data' }}").apply(context);
+    assertThat(
+        output,
+        is(
+            "[ERROR: All sort field values must be of the same comparable type (Number, String, or Boolean)]"));
+  }
+
+  @Test
+  void errorsIfSortFieldHasMixedTypes() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    String input = """
+            [{"value":"text"},{"value":123}]""";
+    Map<String, String> context = new HashMap<>();
+    context.put("input", input);
+    String output = handleBars.compileInline("{{ jsonSort input '$[*].value' }}").apply(context);
+    assertThat(output, containsString("same comparable type"));
+  }
+
+  @Test
+  void errorsIfSortValuesDoNotMatchArraySize() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    // Input has 2 objects, but nested arrays contain 3 total names
+    // $[*].users[*].name will return 3 values for an array of size 2
+    String input =
+        """
+                    [{"users":[{"name":"bob"},{"name":"alice"}]},{"users":[{"name":"charlie"}]}]""";
+    Map<String, String> context = new HashMap<>();
+    context.put("input", input);
+    String output =
+        handleBars.compileInline("{{ jsonSort input '$[*].users[*].name' }}").apply(context);
+    assertThat(output, containsString("Number of sort values"));
+    assertThat(output, containsString("does not match array size"));
+  }
+
+  @Test
+  void sortsArrayByStringFieldAscending() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    String input =
+        """
+            [{"id":456,"name":"bob"},{"id":123,"name":"alice"},{"id":789,"name":"charlie"}]""";
+    String expected =
+        """
+            [{"id":123,"name":"alice"},{"id":456,"name":"bob"},{"id":789,"name":"charlie"}]""";
+    Map<String, String> context = new HashMap<>();
+    context.put("input", input);
+    String output = handleBars.compileInline("{{ jsonSort input '$[*].name' }}").apply(context);
+    assertThat(output, is(expected));
+  }
+
+  @Test
+  void sortsArrayByNumericFieldAscending() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    String input = """
+            [{"id":456},{"id":123},{"id":789},{"id":321}]""";
+    String expected = """
+            [{"id":123},{"id":321},{"id":456},{"id":789}]""";
+    Map<String, String> context = new HashMap<>();
+    context.put("input", input);
+    String output = handleBars.compileInline("{{ jsonSort input '$[*].id' }}").apply(context);
+    assertThat(output, is(expected));
+  }
+
+  @Test
+  void sortsArrayByNumericFieldDescending() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    String input = """
+            [{"id":123},{"id":456},{"id":789}]""";
+    String expected = """
+            [{"id":789},{"id":456},{"id":123}]""";
+    Map<String, String> context = new HashMap<>();
+    context.put("input", input);
+    String output =
+        handleBars.compileInline("{{ jsonSort input '$[*].id' order='desc' }}").apply(context);
+    assertThat(output, is(expected));
+  }
+
+  @Test
+  void sortsArrayByBooleanField() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    String input = """
+            [{"active":true},{"active":false},{"active":true}]""";
+    String expected = """
+            [{"active":false},{"active":true},{"active":true}]""";
+    Map<String, String> context = new HashMap<>();
+    context.put("input", input);
+    String output = handleBars.compileInline("{{ jsonSort input '$[*].active' }}").apply(context);
+    assertThat(output, is(expected));
+  }
+
+  @Test
+  void sortsNestedArrayByField() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    String input =
+        """
+            {"users":[{"name":"charlie"},{"name":"alice"},{"name":"bob"}]}""";
+    String expected =
+        """
+            {"users":[{"name":"alice"},{"name":"bob"},{"name":"charlie"}]}""";
+    Map<String, String> context = new HashMap<>();
+    context.put("input", input);
+    String output =
+        handleBars.compileInline("{{ jsonSort input '$.users[*].name' }}").apply(context);
+    assertThat(output, is(expected));
+  }
+
+  @Test
+  void sortsArrayByDateFieldInIso8601Format() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    // ISO 8601 format dates (same as {{now}} helper output)
+    String input =
+        """
+            [{"id":1,"created":"2025-03-15T14:30:00Z"},{"id":2,"created":"2025-01-10T09:15:00Z"},{"id":3,"created":"2025-12-01T18:45:00Z"}]""";
+    String expected =
+        """
+            [{"id":2,"created":"2025-01-10T09:15:00Z"},{"id":1,"created":"2025-03-15T14:30:00Z"},{"id":3,"created":"2025-12-01T18:45:00Z"}]""";
+    Map<String, String> context = new HashMap<>();
+    context.put("input", input);
+    String output = handleBars.compileInline("{{ jsonSort input '$[*].created' }}").apply(context);
+    assertThat(output, is(expected));
+  }
+
+  @Test
+  void sortsArrayByDateFieldDescending() throws IOException {
+    Handlebars handleBars = getHandlebarsWithJsonSort();
+    // ISO 8601 format dates sorted in descending order (most recent first)
+    String input =
+        """
+            [{"id":1,"created":"2025-03-15T14:30:00Z"},{"id":2,"created":"2025-01-10T09:15:00Z"},{"id":3,"created":"2025-12-01T18:45:00Z"}]""";
+    String expected =
+        """
+            [{"id":3,"created":"2025-12-01T18:45:00Z"},{"id":1,"created":"2025-03-15T14:30:00Z"},{"id":2,"created":"2025-01-10T09:15:00Z"}]""";
+    Map<String, String> context = new HashMap<>();
+    context.put("input", input);
+    String output =
+        handleBars.compileInline("{{ jsonSort input '$[*].created' order='desc' }}").apply(context);
+    assertThat(output, is(expected));
+  }
+
+  private Handlebars getHandlebarsWithJsonSort() {
+    return new Handlebars()
+        .with(EscapingStrategy.NOOP)
+        .registerHelper("jsonSort", new JsonSortHelper());
+  }
+}
---
+++ wiremock-core/src/main/java/com/github/tomakehurst/wiremock/extension/responsetemplating/helpers/JsonSortHelper.java
@@ -0,0 +1,232 @@
+/*
+ * Copyright (C) 2025 Thomas Akehurst
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.tomakehurst.wiremock.extension.responsetemplating.helpers;
+
+import com.github.jknack.handlebars.Options;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.ParseContext;
+import com.jayway.jsonpath.PathNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JsonSortHelper extends HandlebarsHelper<Object> {
+
+  private final ParseContext parseContext = JsonPath.using(HelperUtils.jsonPathConfig);
+
+  @Override
+  public Object apply(Object inputJson, Options options) throws IOException {
+    if (!(inputJson instanceof String)) {
+      return handleError("Input JSON must be a string");
+    }
+    if (inputJson.equals("null")) {
+      // No op
+      return inputJson;
+    }
+    if (options.params.length != 1) {
+      return handleError("A single JSONPath expression parameter must be supplied");
+    }
+    Object jsonPathString = options.param(0);
+    if (!(jsonPathString instanceof String)) {
+      return handleError("JSONPath parameter must be a string");
+    }
+
+    // Extract and validate optional 'order' parameter
+    String order = "asc"; // default to ascending
+    if (options.hash != null) {
+      Object orderParam = options.hash.get("order");
+      if (orderParam != null) {
+        if (!(orderParam instanceof String)) {
+          return handleError("order parameter must be a string");
+        }
+        order = (String) orderParam;
+        if (!order.equals("asc") && !order.equals("desc")) {
+          return handleError("order parameter must be 'asc' or 'desc'");
+        }
+      }
+    }
+    // Extract array path from JsonPath
+    String arrayPath = extractArrayPath((String) jsonPathString);
+    if (arrayPath == null) {
+      return handleError(
+          "JSONPath must include [*] to specify array location (e.g., '$[*].name' or '$.users[*].name')");
+    }
+
+    DocumentContext jsonDocument;
+    try {
+      jsonDocument = parseContext.parse((String) inputJson);
+    } catch (Exception e) {
+      return handleError("Input JSON string is not valid JSON ('" + inputJson + "')", e);
+    }
+    // Read the sort values using the full JsonPath with [*]
+    List<?> sortValues;
+    try {
+      sortValues = jsonDocument.read((String) jsonPathString, List.class);
+    } catch (PathNotFoundException e) {
+      return handleError("JSONPath expression did not match any values ('" + jsonPathString + "')");
+    } catch (Exception e) {
+      return handleError("Invalid JSONPath expression ('" + jsonPathString + "')", e);
+    }
+    // Read the array to sort
+    Object arrayObject;
+    try {
+      arrayObject = jsonDocument.read(arrayPath);
+    } catch (PathNotFoundException e) {
+      return handleError("Array not found at path ('" + arrayPath + "')");
+    } catch (Exception e) {
+      return handleError("Error reading array at path ('" + arrayPath + "')", e);
+    }
+
+    // Validate it's actually an array
+    if (!(arrayObject instanceof List)) {
+      return handleError("JSONPath does not reference an array ('" + arrayPath + "')");
+    }
+
+    @SuppressWarnings("unchecked")
+    List<Object> array = (List<Object>) arrayObject;
+
+    // Validate sort values list matches array size
+    if (sortValues.size() != array.size()) {
+      return handleError(
+          "Number of sort values ("
+              + sortValues.size()
+              + ") does not match array size ("
+              + array.size()
+              + ")");
+    }
+
+    // Handle empty arrays early - nothing to validate or sort
+    if (array.isEmpty()) {
+      return jsonDocument.jsonString();
+    }
+
+    // Check for nulls (missing fields)
+    for (Object value : sortValues) {
+      if (value == null) {
+        return handleError(
+            "All objects in the array must have the sort field specified by JSONPath expression ('"
+                + jsonPathString
+                + "')");
+      }
+    }
+
+    // Validate all values are the same comparable type
+    Class<?> commonType = detectCommonType(sortValues);
+    if (commonType == null) {
+      return handleError(
+          "All sort field values must be of the same comparable type (Number, String, or Boolean)");
+    }
+
+    // Handle single element arrays - validated, but no sorting needed
+    if (array.size() == 1) {
+      return jsonDocument.jsonString();
+    }
+
+    // Create pairs of (array element, sort value)
+    List<SortPair> pairs = new ArrayList<>();
+    for (int i = 0; i < array.size(); i++) {
+      pairs.add(new SortPair(array.get(i), sortValues.get(i)));
+    }
+
+    // Create comparator based on detected type
+    Comparator<Object> valueComparator = createComparator(commonType);
+    if ("desc".equals(order)) {
+      valueComparator = valueComparator.reversed();
+    }
+
+    // Sort pairs by their sort values
+    final Comparator<Object> finalComparator = valueComparator;
+    pairs.sort((a, b) -> finalComparator.compare(a.sortValue, b.sortValue));
+
+    // Extract sorted objects
+    List<Object> sortedArray = pairs.stream().map(p -> p.object).collect(Collectors.toList());
+
+    // Special case: if we're sorting the root array, we can't use set() as JsonPath doesn't allow
+    // setting the root element. Instead, we serialize the sorted array directly.
+    if ("$".equals(arrayPath)) {
+      return parseContext.parse(sortedArray).jsonString();
+    }
+
+    // Update the document with a sorted array
+    jsonDocument.set(arrayPath, sortedArray);
+
+    return jsonDocument.jsonString();
+  }
+
+  private String extractArrayPath(String jsonPath) {
+    // Extract the array path from expressions like "$[*].name" or "$.users[*].priority"
+    int wildcardIndex = jsonPath.indexOf("[*]");
+    if (wildcardIndex == -1) {
+      return null; // Must contain [*]
+    }
+
+    // Return everything before [*]
+    String path = jsonPath.substring(0, wildcardIndex);
+    return path.isEmpty() ? "$" : path;
+  }
+
+  private Class<?> detectCommonType(List<?> values) {
+    if (values.isEmpty()) {
+      return null;
+    }
+
+    Class<?> firstType = getComparableType(values.get(0));
+    if (firstType == null) {
+      return null;
+    }
+
+    for (Object value : values) {
+      Class<?> valueType = getComparableType(value);
+      if (!firstType.equals(valueType)) {
+        return null;
+      }
+    }
+
+    return firstType;
+  }
+
+  private Class<?> getComparableType(Object value) {
+    if (value == null) {
+      return null;
+    }
+    if (Number.class.isAssignableFrom(value.getClass())) {
+      return Number.class;
+    }
+    if (value instanceof String) {
+      return String.class;
+    }
+    if (value instanceof Boolean) {
+      return Boolean.class;
+    }
+    return null; // Unsupported type (object, array, etc.)
+  }
+
+  private record SortPair(Object object, Object sortValue) {}
+
+  private Comparator<Object> createComparator(Class<?> type) {
+    if (Number.class.equals(type)) {
+      return Comparator.comparingDouble(v -> ((Number) v).doubleValue());
+    } else if (String.class.equals(type)) {
+      return Comparator.comparing(v -> (String) v);
+    } else if (Boolean.class.equals(type)) {
+      return Comparator.comparing(v -> (Boolean) v);
+    }
+    throw new IllegalArgumentException("Unsupported type: " + type);
+  }
+}
