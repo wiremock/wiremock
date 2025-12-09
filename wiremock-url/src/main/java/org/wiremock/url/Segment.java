3bdecf7b2d8521e97cb09ad073d466e07754a13c
Robert Elliot <rob@lidalia.org.uk>
2025-12-09 22:33:49 +0000

    Fix a few more bugs

--- wiremock-url/src/main/java/org/wiremock/url/Segment.java
+++ wiremock-url/src/main/java/org/wiremock/url/Segment.java
@@ -1,16 +1,32 @@
 package org.wiremock.url;
 
+import java.net.URLDecoder;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 public interface Segment extends PctEncoded {
   Segment EMPTY = new SegmentImpl("");
   Segment DOT = new SegmentImpl(".");
   Segment DOT_DOT = new SegmentImpl("..");
+
+  default boolean isDot() {
+    return decode().equals(Segment.DOT.toString());
+  }
+
+  default boolean isDotDot() {
+    return decode().equals(Segment.DOT_DOT.toString());
+  }
 }
 
 record SegmentImpl(String stringForm) implements Segment {
 
   @Override
   public String decode() {
-    throw new UnsupportedOperationException();
+    try {
+      return URLDecoder.decode(stringForm, UTF_8);
+    } catch (IllegalArgumentException ignored) {
+      return stringForm;
+    }
   }
 
   @Override
