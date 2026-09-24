77e97382e71a96715392049fa32b3be1da6ec209
Tom Akehurst <tom@wiremock.org>
2026-09-24 14:07:44 +0100

    Fix dependency issue broken suring server-sent-events merge

--- wiremock-jetty/build.gradle.kts
+++ wiremock-jetty/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
 
     implementation(project(":wiremock-core:certificate-generator"))
     implementation(libs.jetty.alpn.server)
+    implementation(libs.jetty.ee11.servlets)
     implementation(libs.jetty.http)
     implementation(libs.jetty.http2.common)
     implementation(libs.jetty.http2.server)
